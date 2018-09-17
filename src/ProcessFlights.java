caa7a72ac4266ba7d1bf54f9146a621022e8d1ff
Travis Desell <tdesell@Traviss-MacBook-Pro-3.local>
2018-09-17 16:06:46 -0400

    Added a compilation script and updated exceedences file for duplicate PA28 issue.

--- src/ProcessFlights.java
+++ src/ProcessFlights.java
@@ -12,7 +12,6 @@ public class ProcessFlights {
         // double backquote is to avoid compiler interpret words
         // like \test as \t (ie. as a escape sequence)
 
-
         System.out.println("Command Line Arguments:");
         for (int i = 0; i < arguments.length; i++) {
             System.out.println("arguments[" + i + "]: '" + arguments[i] + "'");
@@ -23,8 +22,19 @@ public class ProcessFlights {
             System.exit(1);
         }
 
+        //The first argument should be the directory we're going to parse
+        //For each subdirectory:
+        //  get the airframe type (C172, C182, PA28, etc.)
+        //  for each N-Number in that directory:
+        //      for each flight in that directory:
+        //          create the appropriate airframe object (C172 for C172s)
+        //          get events
+        //          insert the events and flight information into the database
+
+
         Airframe airframe = new Airframe(arguments[0]);
         //C172 airframe = new C172(arguments[0]);
+        //PA22 airframe = new PA22(arguments[0]);
         airframe.printInformation();
         airframe.printValues();
 
