8aa2d7743088317850b79fcfee6978091a92a1b8
Travis Desell <tdesell@Traviss-MacBook-Pro-3.local>
2018-09-17 16:06:46 -0400

    Added a compilation script and updated exceedences file for duplicate PA28 issue.

---
+++ compile.sh
@@ -0,0 +1,9 @@
+echo "Compiling ./src/events/*.java"
+javac ./src/events/*.java
+
+echo "Compiling ./src/airframes/*.java"
+javac ./src/airframes/*.java
+
+echo "Compiling ./src/*.java"
+javac ./src/*.java
+
