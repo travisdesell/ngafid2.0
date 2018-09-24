8d3d231e3bb6062727de15b80becd39b13f89501
FredAkh <farhad88.akhbardeh88@gmail.com>
2018-09-24 13:58:27 -0400

    Add files via upload

---
+++ src/events/sr20/SR20LowAirspeedOnClimboutEvent.java
@@ -0,0 +1,35 @@
+package src.events;
+
+import java.util.ArrayList;
+
+//Condition: Low Airspeed on Climbout: indicated_airspeed < 52 AND vertical_airspeed > 0 AND (radio_altitude_derived BETWEEN 100 AND 500)
+public class SR20LowAirspeedOnClimboutEvent extends Event {
+
+    private static final int sr20IndicatedAirspeedColumn = 10;
+    private static final int sr20VerticalAirspeedColumn = 12;
+    private static final int sr20RadioAltitudeColumn = 6;
+    private static final double sr20AirspeedLimit = 52;
+    private static final double sr20VertialAirspeedLimit = 0;
+    private static final double sr20RadioAltitudeDerivedLowLimit = 100;
+    private static final double sr20RadioAltitudeDerivedHighLimit = 500;
+
+    public SR20LowAirspeedOnClimboutEvent(String startTime, String endTime, int startLine, int endLine) {
+        super(startTime, endTime, startLine, endLine, 5);
+    }
+
+    public static boolean isOccuring(ArrayList<String> lineValues) {
+        double sr20IndicatedAirspeedOn = Double.parseDouble(lineValues.get(sr20IndicatedAirspeedColumn));
+        double sr20VerticalAirspeedOn = Double.parseDouble(lineValues.get(sr20VerticalAirspeedColumn));
+        double sr20RadioAltitude = Double.parseDouble(lineValues.get(sr20RadioAltitudeColumn));
+
+        if ((sr20IndicatedAirspeedOn < sr20AirspeedLimit & sr20VerticalAirspeed > sr20VertialAirspeedLimit) && (sr20RadioAltitude < sr20RadioAltitudeDerivedLowLimit & sr20RadioAltitude > sr20RadioAltitudeDerivedHighLimit)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "LOW AIR SPEED ON APPROACH EVENT " + super.toString();
+    }
+}
---
+++ src/events/sr20/SR20LowFuelEvent.java
@@ -0,0 +1,30 @@
+package src.events;
+
+import java.util.ArrayList;
+
+//condition: Low Fuel: N.A.
+public class SR20LowFuelEvent extends Event {
+
+    private static final int sr20LowFuelLeftColumn = 23;
+    private static final int sr20LowFuelRightColumn = 24;
+    private static final double sr20LowFuelLimit = 8;
+
+    public SR20LowFuelEvent(String startTime, String endTime, int startLine, int endLine) {
+        super(startTime, endTime, startLine, endLine, 10);
+    }
+
+    public static boolean isOccuring(ArrayList<String> lineValues) {
+        double sr20LowFuelLeft = Double.parseDouble(lineValues.get(sr20LowFuelLeftColumn));
+        double sr20LowFuelRight = Double.parseDouble(lineValues.get(sr20LowFuelRightColumn));
+
+        if ((sr20LowFuelLeft + sr20LowFuelRight) < sr20LowFuelLimit ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "LOW FUEL EVENT " + super.toString();
+    }
+}
