abff46f49b11064697eca2811b917db4aafb26a9
Farhad Akhbardeh (RIT Student) <fa3019@gol1570-10.main.ad.rit.edu>
2018-10-02 17:52:03 -0400

    update class

--- src/main/java/org/ngafid/events/c172/C172HighCHTEvent.java
+++ src/main/java/org/ngafid/events/c172/C172HighCHTEvent.java
@@ -9,19 +9,26 @@ import org.ngafid.events.Event;
 public class C172HighCHTEvent extends Event {
 
     private final static int c172HighCHTColumn = 29;
-    //private final static int c172HighCHTColumn2 = 30;
-    //private final static int c172HighCHTColumn3 = 31;
-    //private final static int c172HighCHTColumn4 = 32;
+    private final static int c172HighCHTColumn2 = 30;
+    private final static int c172HighCHTColumn3 = 31;
+    private final static int c172HighCHTColumn4 = 32;
 
+    // When I am putting the limit as 250 or less as a limit, the event does not show in the  
     private final static double c172HighCHTLimit = 500;
+    private final static double c172HighCHTLimit2 = 500;
+    private final static double c172HighCHTLimit3 = 500;
+    private final static double c172HighCHTLimit4 = 500;
 
     public C172HighCHTEvent(String startTime, String endTime, int startLine, int endLine) {
-        super(startTime, endTime, startLine, endLine, 10);
+        super(startTime, endTime, startLine, endLine, 5);
     }
 
     public static boolean isOccuring(ArrayList<String> lineValues) {
         double c172HighCHT = Double.parseDouble(lineValues.get(c172HighCHTColumn));
-        if (c172HighCHT > c172HighCHTLimit) {
+        double c172HighCHT2 = Double.parseDouble(lineValues.get(c172HighCHTColumn2));
+        double c172HighCHT3 = Double.parseDouble(lineValues.get(c172HighCHTColumn3));
+        double c172HighCHT4 = Double.parseDouble(lineValues.get(c172HighCHTColumn4));
+        if (c172HighCHT > c172HighCHTLimit || c172HighCHT2 > c172HighCHTLimit2 || c172HighCHT3 > c172HighCHTLimit3 || c172HighCHT4 > c172HighCHTLimit4) {
             return true;
         } else {
             return false;
--- src/main/java/org/ngafid/events/c172/C172LowAirspeedOnApproachEvent.java
+++ src/main/java/org/ngafid/events/c172/C172LowAirspeedOnApproachEvent.java
@@ -9,12 +9,18 @@ import org.ngafid.events.Event;
 public class C172LowAirspeedOnApproachEvent extends Event {
 
     private static final int c172IndicatedAirspeedColumn = 10;
+    private static final double c172AirspeedLimit = 57.0;
+
     private static final int c172VerticalAirspeedColumn = 12;
+    private static final double c172VerticalAirspeedLimit = 0;
+
     private static final int c172RadioAltitudeColumn = 6;
-    private static final double c172AirspeedLimit = 57;
-    private static final double c172VertialAirspeedLimit = 0;
-    private static final double c172RadioAltitudeDerivedLowLimit = 100;
-    private static final double c172RadioAltitudeDerivedHighLimit = 500;
+
+    //private static final double c172RadioAltitudeDerivedLowLimit = 100;
+    // 860 and 861  is only test number. The actual number is line above
+    private static final double c172RadioAltitudeDerivedLowLimit = 860;
+    //private static final double c172RadioAltitudeDerivedHighLimit = 500;
+    private static final double c172RadioAltitudeDerivedHighLimit = 861;
 
     public C172LowAirspeedOnApproachEvent(String startTime, String endTime, int startLine, int endLine) {
         super(startTime, endTime, startLine, endLine, 5);
@@ -22,15 +28,21 @@ public class C172LowAirspeedOnApproachEvent extends Event {
 
     public static boolean isOccuring(ArrayList<String> lineValues) {
         double c172IndicatedAirspeed = Double.parseDouble(lineValues.get(c172IndicatedAirspeedColumn));
+
         double c172VerticalAirspeed = Double.parseDouble(lineValues.get(c172VerticalAirspeedColumn));
+
         double c172RadioAltitude = Double.parseDouble(lineValues.get(c172RadioAltitudeColumn));
 
-        if ((c172IndicatedAirspeed < c172AirspeedLimit && c172VerticalAirspeed < c172VertialAirspeedLimit) && (c172RadioAltitude < c172RadioAltitudeDerivedLowLimit && c172RadioAltitude > c172RadioAltitudeDerivedHighLimit)) {
+
+        if (((c172RadioAltitude > c172RadioAltitudeDerivedLowLimit) && (c172RadioAltitude < c172RadioAltitudeDerivedHighLimit))&& (c172IndicatedAirspeed < c172AirspeedLimit) && (c172VerticalAirspeed < c172VerticalAirspeedLimit)){
             return true;
         } else {
             return false;
         }
+
     }
+    
+    
 
     public String toString() {
         return "LOW AIR SPEED ON APPROACH EVENT " + super.toString();
