614d38ee4c3477119ecea09f49f9b59de3c0d550
Travis Desell <travisdesell@traviss-mbp.wireless.rit.edu>
2018-10-10 10:57:00 -0400

    Now reads db info from db_info.php file (ignored in gitinfo)

--- src/main/java/org/ngafid/ProcessFlights.java
+++ src/main/java/org/ngafid/ProcessFlights.java
@@ -1,5 +1,7 @@
 package org.ngafid;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.IOException;
 
@@ -27,6 +29,62 @@ import org.ngafid.flights.Flight;
 
 public class ProcessFlights {
     private static Connection connection;
+
+    static {
+        if (System.getenv("NGAFID_DB_INFO") == null) {
+            System.err.println("ERROR: 'NGAFID_DB_INFO' environment variable not specified at runtime.");
+            System.err.println("Please add the following to your ~/.bash_rc or ~/.profile file:");
+            System.err.println("export NGAFID_DB_INFO=<path/to/db_info_file>");
+            System.exit(1);
+        }
+        String NGAFID_DB_INFO = System.getenv("NGAFID_DB_INFO");
+
+        String dbHost = "";
+        String dbName = "";
+        String dbUser = "";
+        String dbPassword = "";
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(NGAFID_DB_INFO));
+            bufferedReader.readLine();
+
+            dbUser = bufferedReader.readLine();
+            dbUser = dbUser.substring(dbUser.indexOf("'") + 1);
+            dbUser = dbUser.substring(0, dbUser.indexOf("'"));
+
+            dbName = bufferedReader.readLine();
+            dbName = dbName.substring(dbName.indexOf("'") + 1);
+            dbName = dbName.substring(0, dbName.indexOf("'"));
+
+            dbHost = bufferedReader.readLine();
+            dbHost = dbHost.substring(dbHost.indexOf("'") + 1);
+            dbHost = dbHost.substring(0, dbHost.indexOf("'"));
+
+            dbPassword = bufferedReader.readLine();
+            dbPassword = dbPassword.substring(dbPassword.indexOf("'") + 1);
+            dbPassword = dbPassword.substring(0, dbPassword.indexOf("'"));
+
+            System.out.println("dbHost: '" + dbHost + "'");
+            System.out.println("dbName: '" + dbName + "'");
+            System.out.println("dbUser: '" + dbUser + "'");
+            System.out.println("dbPassword: '" + dbPassword + "'");
+
+        } catch (IOException e) {
+            System.err.println("Error reading from NGAFID_DB_INFO: '" + NGAFID_DB_INFO + "'");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            connection = DriverManager.getConnection("jdbc:mysql://" + dbHost + "/" + dbName, dbUser, dbPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public static void processFlightFile(String filename) {
         try {
@@ -104,30 +162,11 @@ public class ProcessFlights {
             System.out.println("arguments[" + i + "]: '" + arguments[i] + "'");
         }
 
-        if (arguments.length == 0) {
+        if (arguments.length != 1) {
             System.err.println("Incorrect arguments, usage:");
-            System.err.println("java org.ngafid.ProcessFlights <flight csv/zip> [<database file>]");
+            System.err.println("java org.ngafid.ProcessFlights <flight csv/zip>");
             System.exit(1);
         }
-
-        //if (arguments.length == 2) {
-            //second argument is the file for the mysql database information
-
-            String dbHost = "127.0.0.1:13306";
-            String dbName = "ngafid";
-            String dbUser = "ngafid_live";
-            String dbPassword = "pompous paper petunias";
-
-            try {
-                // This will load the MySQL driver, each DB has its own driver
-                Class.forName("com.mysql.jdbc.Driver");
-                // Setup the connection with the DB
-                connection = DriverManager.getConnection("jdbc:mysql://" + dbHost + "/" + dbName, dbUser, dbPassword);
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        //}
 
 
         //The first argument should be the directory we're going to parse
