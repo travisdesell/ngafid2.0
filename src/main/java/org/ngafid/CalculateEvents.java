768905deba48af99eb22865bedde0b5d25b1b9c9
Travis Desell <tdesell@ngafid.und.edu>
2018-10-11 14:18:41 -0500

    Added new event code for grabbing events from database

--- src/main/java/org/ngafid/CalculateEvents.java
+++ src/main/java/org/ngafid/CalculateEvents.java
@@ -7,11 +7,8 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.io.IOException;
 
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 
 import java.util.ArrayList;
@@ -22,14 +19,7 @@ import java.util.zip.ZipFile;
 import org.ngafid.flights.DoubleTimeSeries;
 import org.ngafid.flights.MalformedFlightFileException;
 
-import org.ngafid.airframes.Airframe;
-import org.ngafid.airframes.C172;
-import org.ngafid.airframes.C182;
-import org.ngafid.airframes.PA28;
-import org.ngafid.airframes.PA44;
-import org.ngafid.airframes.SR20;
-
-import org.ngafid.events.Event;
+import org.ngafid.events2.PitchEvent;
 
 import org.ngafid.flights.Flight;
 
@@ -97,29 +87,6 @@ public class CalculateEvents {
         // double backquote is to avoid compiler interpret words
         // like \test as \t (ie. as a escape sequence)
 
-        String columnName = "Pitch";
-        double minValue = -30.0;
-        double maxValue = 30.0;
-        String query = "SELECT id, flight_id, name, length, valid_length, min, avg, max, `values` FROM double_series WHERE name = ? AND (min <= ? OR max >= ?)";
-
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-        System.out.println(preparedStatement);
-        preparedStatement.setString(1, columnName);
-        preparedStatement.setDouble(2, minValue);
-        preparedStatement.setDouble(3, maxValue);
-        System.out.println(preparedStatement);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()) {
-            DoubleTimeSeries timeSeries = new DoubleTimeSeries(resultSet);
-
-            System.out.println(timeSeries);
-            System.out.println();
-            System.out.println();
-        }
-
-        System.err.println("finished!");
+        PitchEvent.calculateEvents(connection);
     }
 }
