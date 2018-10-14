37a0218b6b94baf7b83a218ec0c86f66c57dee05
Farhad Akhbardeh <fakhbardeh@ngafid.und.edu>
2018-10-13 21:28:58 -0500

    Some Events Changed

--- src/main/java/org/ngafid/events/PitchEvent.java
+++ src/main/java/org/ngafid/events/PitchEvent.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class PitchEvent extends Event {
 
-    private final static int pitchColumn = 13;
-    private final static double pitchLimit = 10.0;
+   private final static int pitchColumn = 13;
+   private final static double pitchLimit = 10.0;
 
-    public PitchEvent(String startTime, String endTime, int startLine, int endLine) {
-        super(startTime, endTime, startLine, endLine, 10);
-    }
+   public PitchEvent(String startTime, String endTime, int startLine, int endLine) {
+       super(startTime, endTime, startLine, endLine, 10);
+   }
 
-    public static boolean isOccuring(ArrayList<String> lineValues) {
-        double pitch = Double.parseDouble(lineValues.get(pitchColumn));
-        if (Math.abs(pitch) > pitchLimit) {
+   public static boolean isOccuring(ArrayList<String> lineValues) {
+       double pitch = Double.parseDouble(lineValues.get(pitchColumn));
+       if (Math.abs(pitch) > pitchLimit) {
             return true;
-        } else {
-            return false;
-        }
-    }
+      } else {
+           return false;
+       }
+   }
 
-    public String toString() {
-        return "PITCH EVENT " + super.toString();
-    }
+   public String toString() {
+       return "PITCH EVENT " + super.toString();
+   }
 }
