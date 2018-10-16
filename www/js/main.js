bca017016d433158c0f502e16d1b2d38af3cc97f
Travis Desell <tdesell@ngafid.und.edu>
2018-10-16 13:45:09 -0500

    Updated pitch event.

--- www/js/main.js
+++ www/js/main.js
@@ -99,6 +99,32 @@ $(document).ready(function() {
         //user_id : user_id
     };   
 
+    var trace1 = { 
+            x: [1, 2, 3, 4], 
+            y: [10, 15, 13, 17], 
+            type: 'scatter'
+    };
+    var trace2 = { 
+            x: [1, 2, 3, 4], 
+            y: [16, 5, 11, 9], 
+            type: 'scatter'
+    };
+    var data = [trace1, trace2];
+
+    var layout1 = { 
+            yaxis: {rangemode: 'tozero',
+                        showline: true,
+                        zeroline: true}
+    };
+
+    var layout2 = { 
+            yaxis: {rangemode: 'tozero',
+                        zeroline: true}
+    };
+
+    Plotly.newPlot('div1', data, layout1);
+
+
     $.ajax({
         type: 'POST',
         url: './request.php',
