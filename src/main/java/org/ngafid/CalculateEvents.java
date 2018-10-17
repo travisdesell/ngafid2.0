3b240bc7fe9c28493387be17f4866231941936c3
Farhad Akhbardeh <fakhbardeh@ngafid.und.edu>
2018-10-17 09:13:49 -0500

    update class events

--- src/main/java/org/ngafid/CalculateEvents.java
+++ src/main/java/org/ngafid/CalculateEvents.java
@@ -25,8 +25,46 @@ import org.ngafid.events2.LateralAccelerationEvent;
 import org.ngafid.events2.LongitudinalAccelerationEvent;
 import org.ngafid.events2.VerticalAccelerationEvent;
 import org.ngafid.events2.VsiOnFinalEvent;
+
 import org.ngafid.events2.c172Cessna.C172HighAltitudeEvent;
+import org.ngafid.events2.c172Cessna.C172HighCHTEvent;
+import org.ngafid.events2.c172Cessna.C172IndicatedAirspeedEvent;
+import org.ngafid.events2.c172Cessna.C172LowAirspeedOnApproachEvent;
+import org.ngafid.events2.c172Cessna.C172LowAirspeedOnClimboutEvent;
+import org.ngafid.events2.c172Cessna.C172LowFuelEvent;
+import org.ngafid.events2.c172Cessna.C172LowOilPressureEvent;
+
 import org.ngafid.events2.c182Cessna.C182HighAltitudeEvent;
+import org.ngafid.events2.c182Cessna.C182HighCHTEvent;
+import org.ngafid.events2.c182Cessna.C182IndicatedAirspeedEvent;
+import org.ngafid.events2.c182Cessna.C182LowAirspeedOnApproachEvent;
+import org.ngafid.events2.c182Cessna.C182LowAirspeedOnClimboutEvent;
+import org.ngafid.events2.c182Cessna.C182LowFuelEvent;
+import org.ngafid.events2.c182Cessna.C182LowOilPressureEvent;
+
+import org.ngafid.events2.pa28Piper.PA28HighAltitudeEvent;
+import org.ngafid.events2.pa28Piper.PA28HighCHTEvent;
+import org.ngafid.events2.pa28Piper.PA28IndicatedAirspeedEvent;
+import org.ngafid.events2.pa28Piper.PA28LowAirspeedOnApproachEvent;
+import org.ngafid.events2.pa28Piper.PA28LowAirspeedOnClimboutEvent;
+import org.ngafid.events2.pa28Piper.PA28LowFuelEvent;
+import org.ngafid.events2.pa28Piper.PA28LowOilPressureEvent;
+
+import org.ngafid.events2.pa44Piper.PA44HighAltitudeEvent;
+import org.ngafid.events2.pa44Piper.PA44HighCHTEvent;
+import org.ngafid.events2.pa44Piper.PA44IndicatedAirspeedEvent;
+import org.ngafid.events2.pa44Piper.PA44LowAirspeedOnApproachEvent;
+import org.ngafid.events2.pa44Piper.PA44LowAirspeedOnClimboutEvent;
+import org.ngafid.events2.pa44Piper.PA44LowFuelEvent;
+import org.ngafid.events2.pa44Piper.PA44LowOilPressureEvent;
+
+import org.ngafid.events2.sr20Cirrus.SR20HighAltitudeEvent;
+import org.ngafid.events2.sr20Cirrus.SR20HighCHTEvent;
+import org.ngafid.events2.sr20Cirrus.SR20IndicatedAirspeedEvent;
+import org.ngafid.events2.sr20Cirrus.SR20LowAirspeedOnApproachEvent;
+import org.ngafid.events2.sr20Cirrus.SR20LowAirspeedOnClimboutEvent;
+import org.ngafid.events2.sr20Cirrus.SR20LowFuelEvent;
+import org.ngafid.events2.sr20Cirrus.SR20LowOilPressureEvent;
 
 import org.ngafid.flights.Flight;
 
@@ -100,7 +138,46 @@ public class CalculateEvents {
         LongitudinalAccelerationEvent.calculateEvents(connection);
         VerticalAccelerationEvent.calculateEvents(connection);
         VsiOnFinalEvent.calculateEvents(connection);
+
         C172HighAltitudeEvent.calculateEvents(connection);
+        C172HighCHTEvent.calculateEvents(connection);
+        C172IndicatedAirspeedEvent.calculateEvents(connection);
+        C172LowAirspeedOnApproachEvent.calculateEvents(connection);
+        C172LowAirspeedOnClimboutEvent.calculateEvents(connection);
+        C172LowFuelEvent.calculateEvents(connection);
+        C172LowOilPressureEvent.calculateEvents(connection);
+
         C182HighAltitudeEvent.calculateEvents(connection);
+        C182HighCHTEvent.calculateEvents(connection);
+        C182IndicatedAirspeedEvent.calculateEvents(connection);
+        C182LowAirspeedOnApproachEvent.calculateEvents(connection);
+        C182LowAirspeedOnClimboutEvent.calculateEvents(connection);
+        C182LowFuelEvent.calculateEvents(connection);
+        C182LowOilPressureEvent.calculateEvents(connection);
+
+        PA28HighAltitudeEvent.calculateEvents(connection);
+        PA28HighCHTEvent.calculateEvents(connection);
+        PA28IndicatedAirspeedEvent.calculateEvents(connection);
+        PA28LowAirspeedOnApproachEvent.calculateEvents(connection);
+        PA28LowAirspeedOnClimboutEvent.calculateEvents(connection);
+        PA28LowFuelEvent.calculateEvents(connection);
+        PA28LowOilPressureEvent.calculateEvents(connection);
+
+        PA44HighAltitudeEvent.calculateEvents(connection);
+        PA44HighCHTEvent.calculateEvents(connection);
+        PA44IndicatedAirspeedEvent.calculateEvents(connection);
+        PA44LowAirspeedOnApproachEvent.calculateEvents(connection);
+        PA44LowAirspeedOnClimboutEvent.calculateEvents(connection);
+        PA44LowFuelEvent.calculateEvents(connection);
+        PA44LowOilPressureEvent.calculateEvents(connection);
+
+        SR20HighAltitudeEvent.calculateEvents(connection);
+        SR20HighCHTEvent.calculateEvents(connection);
+        SR20IndicatedAirspeedEvent.calculateEvents(connection);
+        SR20LowAirspeedOnApproachEvent.calculateEvents(connection);
+        SR20LowAirspeedOnClimboutEvent.calculateEvents(connection);
+        SR20LowFuelEvent.calculateEvents(connection);
+        SR20LowOilPressureEvent.calculateEvents(connection);
+
     }
 }
