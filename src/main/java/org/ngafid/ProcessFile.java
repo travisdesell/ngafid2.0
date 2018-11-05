c3e1ead2a824fa5fa438c12c8d9cd659fe960161
Travis Desell <travisdesell@Traviss-MBP.wireless.rit.edu>
2018-11-05 13:50:17 -0500

    Updates for testing event checking

--- src/main/java/org/ngafid/ProcessFile.java
+++ src/main/java/org/ngafid/ProcessFile.java
@@ -106,14 +106,17 @@ public class ProcessFile {
     }
 
     public static void main(String[] arguments) {
-        //String filename = "/Users/fa3019/Code/NGAFID/ngafid2.0/example_data/C172/log_110812_095915_KCKN.csv";
+        String filename = "/Users/fa3019/Code/NGAFID/ngafid2.0/example_data/C172/log_110812_095915_KCKN.csv";
         // String filename = "./example_data/C172/log_110812_095915_KCKN.csv";
 
-        File folder = new File("/Users/travisdesell/Data/ngafid/und_single_week/C172/N507ND/");
-        File[] listOfFiles = folder.listFiles();
+        //File folder = new File("/Users/travisdesell/Data/ngafid/und_single_week/C172/N507ND/");
+        //File[] listOfFiles = folder.listFiles();
 
         long startMillis = System.currentTimeMillis();
 
+        testFile(fileanme);
+
+        /*
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
                 String filename = listOfFiles[i].toString();
@@ -122,6 +125,8 @@ public class ProcessFile {
                 testFile(filename);
             }
         }
+        */
+
         long endMillis = System.currentTimeMillis();
 
         System.out.println("It took " + (endMillis - startMillis) + " ms to run the code");
