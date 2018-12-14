9d91b1d9b6d456f68042d5f3b3d612f63c3e52b1
Travis Desell <travis.desell@gmail.com>
2018-12-14 12:48:05 -0600

    Moved into DB directory as we're not using PHP for the webpages anymore

--- db/create_tables.php
+++ db/create_tables.php
@@ -4,7 +4,7 @@ $cwd[__FILE__] = __FILE__;
 if (is_link($cwd[__FILE__])) $cwd[__FILE__] = readlink($cwd[__FILE__]);
 $cwd[__FILE__] = dirname($cwd[__FILE__]);
 
-require_once($cwd[__FILE__] . "/../www_php/my_query.php");
+require_once($cwd[__FILE__] . "/my_query.php");
 
 $drop_tables = false;
 
