64dcba454f749233da1b8bbcf225816d8b8e6e88
Farhad Akhbardeh (RIT Student) <fa3019@gol1570-10.main.ad.rit.edu>
2019-02-06 10:56:03 -0500

    Event generater added

---
+++ src/main/java/org/ngafid/events_db/Condition.java
@@ -0,0 +1,51 @@
+
+package org.ngafid.events_db;
+
+import java.util.Scanner;
+
+// current < minValue || current > maxValue
+//current1 || current2 
+
+class Condition {
+    public static void main(String[] args) {
+
+    	char operator;
+      Double current1, current2;
+      String rull, result;
+
+    	Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter operator (either || or &&): ");
+      
+    	operator = scanner.next().charAt(0);
+    	System.out.print("Enter current1, rull and current2 respectively: ");
+      current1 = scanner.nextDouble();
+      rull = scanner.nextString();
+    	current2 = scanner.nextDouble();
+
+    	switch (operator) {
+        //  case '||':
+        //    result = current1 +rull+ current2;
+    	  //  System.out.print(current1 + "number1" + rull "||"+ current2 + " number2 " + result);
+        //    break;
+
+         case '-':
+           result = current1 - current2;
+           System.out.print(current1 + "-" + current2 + " = " + result);
+           break;
+
+         case '*':
+           result = current1 * current2;
+           System.out.print(current1 + "*" + current2 + " = " + result);
+           break;
+
+         case '/':
+           result = current1 / current2;
+           System.out.print(current1 + "/" + current2 + " = " + result);
+           break;
+
+         default:
+           System.out.println("Invalid operator!");
+           break;
+        }
+    }
+}
