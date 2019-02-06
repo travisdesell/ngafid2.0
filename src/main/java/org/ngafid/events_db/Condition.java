3129798af1a34e56ec2d49386d02c34997319096
Travis Desell <travis.desell@gmail.com>
2019-02-06 10:19:45 -0600

    Reformatted and added a couple things to Condition

--- src/main/java/org/ngafid/events_db/Condition.java
+++ src/main/java/org/ngafid/events_db/Condition.java
@@ -7,45 +7,54 @@ import java.util.Scanner;
 //current1 || current2 
 
 class Condition {
+    char operator;
+    double min;
+    double max;
+
+    Condition(String conditionString) {
+        //use scanner to process conditionString and set up 
+        //this object
+    }
+
     public static void main(String[] args) {
 
-    	char operator;
-      Double current1, current2, result;
-      //String rull, result;
+        char operator;
+        Double current1, current2, result;
+        //String rull, result;
 
-    	Scanner scanner = new Scanner(System.in);
-      System.out.print("Enter operator (either || or &&): ");
-      
-    	operator = scanner.next().charAt(0);
-    	System.out.print("Enter current1, rull and current2 respectively: ");
-      current1 = scanner.nextDouble();
-      //rull = scanner.nextString();
-    	current2 = scanner.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter operator (either || or &&): ");
 
-    	switch (operator) {
-        //  case '||':
-        //    result = current1 +rull+ current2;
-    	  //  System.out.print(current1 + "number1" + rull "||"+ current2 + " number2 " + result);
-        //    break;
+        operator = scanner.next().charAt(0);
+        System.out.print("Enter current1, rull and current2 respectively: ");
+        current1 = scanner.nextDouble();
+        //rull = scanner.nextString();
+        current2 = scanner.nextDouble();
 
-         case '-':
-           result = current1 - current2;
-           System.out.print(current1 + "-" + current2 + " = " + result);
-           break;
+        switch (operator) {
+            //  case '||':
+            //    result = current1 +rull+ current2;
+            //  System.out.print(current1 + "number1" + rull "||"+ current2 + " number2 " + result);
+            //    break;
 
-         case '*':
-           result = current1 * current2;
-           System.out.print(current1 + "*" + current2 + " = " + result);
-           break;
+            case '-':
+                result = current1 - current2;
+                System.out.print(current1 + "-" + current2 + " = " + result);
+                break;
 
-         case '/':
-           result = current1 / current2;
-           System.out.print(current1 + "/" + current2 + " = " + result);
-           break;
+            case '*':
+                result = current1 * current2;
+                System.out.print(current1 + "*" + current2 + " = " + result);
+                break;
 
-         default:
-           System.out.println("Invalid operator!");
-           break;
+            case '/':
+                result = current1 / current2;
+                System.out.print(current1 + "/" + current2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
         }
     }
 }
