1d046cf93bfd7d6bea728ccd833983006817d62a
Travis Desell <travis.desell@gmail.com>
2019-02-26 10:25:52 -0600

    First commit of new webpage files

---
+++ src/main/java/org/ngafid/routes/GetWaiting.java
@@ -0,0 +1,76 @@
+package org.ngafid.routes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.HashMap;
+
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
+import spark.Route;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import org.ngafid.Database;
+import org.ngafid.WebServer;
+import org.ngafid.accounts.User;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+
+public class GetWaiting implements Route {
+    private static final Logger LOG = Logger.getLogger(GetWaiting.class.getName());
+    private Gson gson;
+
+    public GetWaiting(Gson gson) {
+        this.gson = gson;
+
+        LOG.info("post " + this.getClass().getName() + " initalized");
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.info("handling " + this.getClass().getName() + " route");
+
+        String resultString = "";
+        String templateFile = WebServer.MUSTACHE_TEMPLATE_DIR + "waiting.html";
+        LOG.severe("template file: '" + templateFile + "'");
+
+        try  {
+            MustacheFactory mf = new DefaultMustacheFactory();
+            Mustache mustache = mf.compile(templateFile);
+
+            HashMap<String, Object> scopes = new HashMap<String, Object>();
+
+            /*
+            List<Item> items = Arrays.asList(
+                    new Item("Travis", "3.00"),
+                    new Item("Shannon", "300.00"),
+                    new Item("Momo", "30.00")
+                    );
+
+            scopes.put("items", items);
+            */
+
+            StringWriter stringOut = new StringWriter();
+            mustache.execute(new PrintWriter(stringOut), scopes).flush();
+            resultString = stringOut.toString();
+
+        } catch (IOException e) {
+            LOG.severe(e.toString());
+        }
+
+        return resultString;
+    }
+}
