02f334e5e5b096f0c1340aee6763ff8d325ca268
apl1341 <apl1341@rit.edu>
2020-05-06 16:31:57 -0400

    abstractified(is this a word?) paginator

---
+++ src/main/java/org/ngafid/common/UploadPaginator.java
@@ -0,0 +1,13 @@
+public class UploadPaginator extends Paginator{
+
+    private int fleetID;
+
+    public UploadPaginator(int bufferSize, int fleetID){
+        super(bufferSize);
+        this.fleetID = fleetID;
+    }
+
+    public Page currentPage(){
+
+    }
+}
