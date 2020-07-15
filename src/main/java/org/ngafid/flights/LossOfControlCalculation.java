590ac66b3e3dc58a3ba33f0baef140a26b5b76b5
apl1341 <apl1341@rit.edu>
2020-07-15 15:49:19 -0400

    added skeleton class for LOC

---
+++ src/main/java/org/ngafid/flights/LossOfControlCalculation.java
@@ -0,0 +1,20 @@
+/**
+ * Loss of control calculator for calculating exceedences
+ * @author <a href = mailto:apl1341@cs.rit.edu>Aidan LaBella @ RIT CS</a>
+ */
+
+package org.ngafid.flights;
+
+public class LossOfControlCalculation{
+	public LossOfControlCalculation(){
+	}
+
+	/**
+	 * Return a floating-point percentage of the probability of loss of control
+	 */
+	public double calculate(){
+		//TODO: implement the caluclation logic here and put parts of the calc. in helper methods 
+		return 0.0;
+	}
+
+}
