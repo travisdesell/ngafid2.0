31cb39d800567d79532ecd4440a5801a50b5da5f
apl1341 <apl1341@rit.edu>
2020-08-26 11:23:16 -0400

    refactored constants

--- src/main/java/org/ngafid/flights/LossOfControlParameters.java
+++ src/main/java/org/ngafid/flights/LossOfControlParameters.java
@@ -6,16 +6,19 @@ public interface LossOfControlParameters{
 	public static final double AOACrit = 15;
 	public static final double proSpinLim = 4;
 
+	public static final String HDG = "HDG";
+	public static final String IAS = "IAS";
+	public static final String VSPD = "VSPD";
+	public static final String OAT = "OAT";
+	public static final String BARO_A = "BaroA";
+	public static final String PITCH = "Pitch";
+	public static final String ROLL = "Roll";
+	public static final String ALT_AGL = "AltAGL";
+
 	/**
 	 * Strings that represent the parameters used in this calculation
 	 **/
-	public static final String [] dtsParamStrings = {"HDG",
-													"IAS",
-													"VSPD",
-													"OAT",
-													"BaroA",
-													"Pitch",
-													"Roll",
-													"AltAGL"};
+	public static final String [] dtsParamStrings = {HDG, IAS, VSPD, OAT, BARO_A,
+														PITCH, ROLL, ALT_AGL};
 }
 
