8b77eff43a8df27bd51f92fb41a6f11066c43c4a
apl1341 <apl1341@rit.edu>
2020-09-22 21:03:04 -0400

    finalized the csv generation for both uncompressed and compressed directories (*.zip)

--- src/main/java/org/ngafid/GenerateBulkCSVS.java
+++ src/main/java/org/ngafid/GenerateBulkCSVS.java
@@ -1,11 +1,16 @@
+/**
+ * Generates/Copies Bulk CSV files for flights in the ngafid
+ *
+ * @author <a href = "mailto:apl1341@cs.rit.edu">Aidan LaBella</a>
+ */
+
 package org.ngafid;
 
 import org.ngafid.common.CSVWriter;
 import org.ngafid.flights.Flight;
 import org.ngafid.filters.Filter;
 
-import java.io.FileWriter;
-import java.io.File;
+import java.io.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,6 +18,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.List;
+
+import java.util.zip.*;
 
 public class GenerateBulkCSVS {
 	private String outDirectoryRoot, uploadDirectoryRoot;
@@ -41,6 +48,14 @@ public class GenerateBulkCSVS {
 		this.displayInfo();
 	}
 
+	/**
+	 * Constructor
+	 *
+	 * @param outDirectoryRoot the root directory of the output file(s)
+	 * @param useZip indicated if all files will be exported in a zip file
+	 * @param startDate the start date to use
+	 * @param endDate the end tdate to use
+	 */
 	public GenerateBulkCSVS(String outDirectoryRoot, int fleetId, boolean useZip, String startDate, String endDate) {
 		this.outDirectoryRoot = outDirectoryRoot;
 		this.fleetId = fleetId;
@@ -48,6 +63,12 @@ public class GenerateBulkCSVS {
 		this.displayInfo();
 	}
 
+	/**
+	 * Querys the database and gets ids by date
+	 *
+	 * @param startDate the start date to use
+	 * @param endDate the end tdate to use
+	 */
 	private void getIdsByDate(String startDate, String endDate) {
 		//"inputs":["Start Date",">=","2020-09-07"]},{"type":"RULE","inputs":["End Date","<=","2020-09-28"]
 		ArrayList<String> startInputs = new ArrayList<>();
@@ -85,7 +106,8 @@ public class GenerateBulkCSVS {
 	 * Info function for command line usage
 	 */
 	public static void usage() {
-		System.err.println("Generate Bulk CSVS");
+		System.err.println("Usage: generate_csvs -f (fleet id) -o (output directory root) [-d date range YYYY-MM-DD to YYYY-MM-DD]\n" +
+							"[-z (enable zip arciving)] [-r range lwr upr (flight numbers)]");
 	}
    
 	/**
@@ -110,6 +132,48 @@ public class GenerateBulkCSVS {
 			}
 		}
 	}
+
+	/**
+	 * Generates files but places them in a ZIP archive
+	 */
+	public void generateToZip() {
+		File file = new File(this.outDirectoryRoot+"flights_" + flights.get(0).getId()
+				+ "_" + flights.get(flights.size() - 1).getId() + ".zip");
+
+		try {
+			if(!file.exists()){
+				System.out.println("Creating new file: "+file);
+				file.createNewFile();
+			}
+
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+			ZipOutputStream zipOut = new ZipOutputStream(bos);
+
+			for (Flight flight : flights) {
+				try{
+					int uploaderId = flight.getUploaderId(); 
+					this.uploadDirectoryRoot = WebServer.NGAFID_ARCHIVE_DIR + "/" + flight.getFleetId() + "/" +
+						uploaderId + "/";
+
+					CSVWriter csvWriter = new CSVWriter(this.uploadDirectoryRoot, flight);
+					ZipEntry zipEntry = csvWriter.getFlightEntry();
+
+					zipOut.putNextEntry(zipEntry);
+					zipOut.write(csvWriter.toBinaryData());
+
+					zipOut.closeEntry();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+			zipOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
 	
 	/**
 	 * {@inheritDoc}
@@ -125,6 +189,11 @@ public class GenerateBulkCSVS {
 	 * @param args cmdline args
 	 */
 	public static void main(String[] args) {
+		if (args[0].equals("-h") || args[0].equals("--help") || args[0].equals("-help") || args[0].equals("--h")) {
+			usage();
+			System.exit(0);
+		}
+
 		String dir = null;
 		int lwr = -1, upr = -1;
 		int fleetId = -1;
@@ -189,7 +258,12 @@ public class GenerateBulkCSVS {
 			gb = new GenerateBulkCSVS(dir, fleetId, zip, lwr, upr);
 		}
 
-		gb.generate();
+		if(zip){
+			gb.generateToZip();
+		} else {
+			gb.generate();
+		}
+
 		System.out.println("done!");
 		System.exit(0);
 	}
