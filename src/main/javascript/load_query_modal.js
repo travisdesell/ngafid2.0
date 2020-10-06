3a606368c58037c0893975740cde9744055a25db
apl1341 <apl1341@rit.edu>
2020-10-06 18:04:01 -0400

    Revert "added queryText area and formatted. Cant get the modal state to update w/ queryText..."
    This reverts commit 4f67a85133299b274fb71472182a4cf217bdb44d.

--- src/main/javascript/load_query_modal.js
+++ src/main/javascript/load_query_modal.js
@@ -148,9 +148,7 @@ class LoadQueriesModal extends React.Component {
                             <label htmlFor="queryText" style={labelStyle}>Full Query:</label>
                         </div>
                         <div className="p-2 flex-fill" style={formHeaderStyle}>
-                            <textarea id="country" className="form-control" name="queryText" rows="5" cols="200" wrap="soft" readOnly>
-                                {"Oh Lookie at all the Query Details. Fancy Fancy. Oh, I wonder if the text is gonna wrap. It'd be such a tragedy if the text left the textarea"}
-                            </textarea>
+                            <input type="text" id="country" name="country" value="Norway" readonly></input>
                         </div>
                     </div>
 
