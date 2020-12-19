2a05a5fd54e4e127f1f78bd43738a01c8f0fc359
apl1341 <apl1341@rit.edu>
2020-12-18 21:24:01 -0500

    added VSI Linear Regression for VSPD calculations

--- src/main/java/org/ngafid/flights/VSPDCalculation.java
+++ src/main/java/org/ngafid/flights/VSPDCalculation.java
@@ -22,28 +22,68 @@ public class VSPDCalculation extends Calculation {
         System.out.println("Calcuating VSPD from MSL data");
 
         DoubleTimeSeries msl = this.parameters.get(ALT_MSL);
+        DoubleTimeSeries mslLagged = msl.lag(DIFF);
         DoubleTimeSeries mslLead = msl.lead(DIFF);
         this.parameters.put(mslLead.getName(), mslLead);
 
         DoubleTimeSeries vsiCalculated = new DoubleTimeSeries(VSPD_CALCULATED, "double");
         
-        //for (int i = 0; i < DIFF; i++) vsiCalculated.add(Double.NaN);
-
         for (int i = DIFF; i < msl.size(); i++) {
-            //double m = msl.get(i) - mslLead.get(i);
-            double m = mslLead.get(i) - msl.get(i);
-            double vsi = m / DIFF;
+            if (i < 1 || i >= msl.size() - 1) {
+                vsiCalculated.add(Double.NaN);
+            } else {
+                double [] yValues = new double[3];
+                double [] xValues = new double[3];
 
-            vsi *= FPM_CONV;
+                xValues[0] = i - 1;
+                xValues[1] = i;
+                xValues[2] = i + 1;
 
-            vsiCalculated.add(vsi);
+                yValues[0] = mslLagged.get(i);
+                yValues[1] = msl.get(i);
+                yValues[2] = mslLead.get(i);
+
+                //TODO: filter out bad readings here
+
+                double m = vsiLinearRegression(xValues, yValues);
+                double vsi = m / DIFF;
+
+                vsi *= FPM_CONV;
+
+                vsiCalculated.add(vsi);
+            }
         }
-
-        vsiCalculated.add(Double.NaN);
 
         this.parameters.put(VSPD_CALCULATED, vsiCalculated);
     }
 
+    public static double average(double ... yValues) {
+        double sum = 0.f;
+
+        for (int i = 0; i < yValues.length; i++) {
+            sum += yValues[i];
+        }
+
+        return sum / yValues.length;
+    }
+
+    public static double vsiLinearRegression(double [] xValues, double [] yValues) {
+        double n = 0.f;
+        double d = 0.f;
+        double xA = average(xValues);
+        double yA = average(yValues);
+
+        assert yValues.length == xValues.length;
+
+        for (int i = 0; i < yValues.length; i++) {
+            double chi = (xValues[i] - xA);
+            n += chi * (yValues[i] - yA);
+            d += (chi * chi);
+        }
+
+        return n / d;
+    }
+            
     public void updateDatabase() {
         DoubleTimeSeries vsi = this.parameters.get(VSPD_CALCULATED);
         vsi.updateDatabase(Database.getConnection(), this.flight.getId());
