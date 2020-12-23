f18e99207a6cace52306e44d8e5ed8740efbef96
apl1341 <apl1341@rit.edu>
2020-12-22 22:49:48 -0500

    db update bug fix

--- src/main/java/org/ngafid/flights/CalculatedDoubleTimeSeries.java
+++ src/main/java/org/ngafid/flights/CalculatedDoubleTimeSeries.java
@@ -7,10 +7,16 @@
 
 package org.ngafid.flights;
 
+import java.sql.Connection;
+import java.util.Map;
+
 import org.ngafid.Database;
 
 public class CalculatedDoubleTimeSeries extends DoubleTimeSeries {
+    static Connection connection = Database.getConnection();
+
     private Calculation calculation;
+    private String [] depNames;
 
     /**
      * Default Constructor
@@ -20,9 +26,10 @@ public class CalculatedDoubleTimeSeries extends DoubleTimeSeries {
      *
      * @param calculation the calculation to perform
      */
-    public CalculatedDoubleTimeSeries(Calculation calculation) {
+    public CalculatedDoubleTimeSeries(Calculation calculation, String [] depNames) {
         super(calculation.getCalculationName(), "double");
         this.calculation = calculation;
+        this.depNames = depNames;
 
         runCalculation();
     }
@@ -34,13 +41,31 @@ public class CalculatedDoubleTimeSeries extends DoubleTimeSeries {
         int flightId = calculation.getFlightId();
         if (!calculation.isNotCalculatable()) {
             System.out.println("Performing " + calculation.getCalculationName() + " calculation on flight #" + flightId);
+
             calculation.calculate(this);
 
-            super.updateDatabase(Database.getConnection(), flightId);
+            updateDatabase(connection, flightId);
         } else {
             System.err.println("WARNING: flight #" + flightId + " is not calculatable for " + calculation.getCalculationName() + "!");
         }
     }
+
+    /**
+     * Updates the database and its dependencies 
+     *
+     * @param connection the database {@link Connection}
+     * @param flightId the flight ID
+     */
+    @Override
+    public void updateDatabase(Connection connection, int flightId) {
+        super.updateDatabase(connection, flightId);
+        Map<String, DoubleTimeSeries> params = calculation.getParameters();
+
+        for (String name : depNames) { 
+            params.get(name).updateDatabase(connection, flightId);
+        }
+    }
+
 
     public boolean notCalculated() {
         return this.calculation.isNotCalculatable();
