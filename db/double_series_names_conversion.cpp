8a7c57ac215eff868e023d5fe70957daeed73060
aidanlabella <apl1341@rit.edu>
2021-06-30 10:09:05 -0400

    added db conversion script for new double_series_names table

---
+++ db/double_series_names_conversion.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+/* uncomment for applications that use vectors */
+/*#include <vector>*/
+
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+using namespace sql;
+
+int main(int c, char** argv) {
+    cout << "Whats up" << endl;
+}
