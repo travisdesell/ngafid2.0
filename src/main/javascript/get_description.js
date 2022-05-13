0c33eb1a6bf9f57219e32bc0a74632ac39b06b8e
Aaron <aarchan108@gmail.com>
2022-05-13 10:02:44 -0400

    Implement a map to store event descriptions to prevent sending constant requests to server

--- src/main/javascript/get_description.js
+++ src/main/javascript/get_description.js
@@ -1,30 +1,39 @@
 import {errorModal} from "./error_modal";
 
+var descriptions = {};
+
+
 export default function GetDescription(eventName) {
+    if (descriptions[eventName]) {
+        console.log("Description for " + eventName + " already stored.");
+        return descriptions[eventName];
+    }
+
     var text = "";
 
-        $.ajax({
-                type: 'GET',
-                url: '/protected/get_event_description',
-                data : {eventName : eventName},
-                dataType : 'json',
-                success : function(response) {
-                    console.log("received response: " + response);
+    console.log("Getting description for " + eventName + " from server.");
+    $.ajax({
+            type: 'GET',
+            url: '/protected/get_event_description',
+            data : {eventName : eventName},
+            dataType : 'json',
+            success : function(response) {
+                console.log("received response: " + response);
 
-                    $('#loading').hide();
+                $('#loading').hide();
 
-                    if (response.err_msg) {
-                        errorModal.show(response.err_title, response.err_msg);
-                    }
+                if (response.err_msg) {
+                    errorModal.show(response.err_title, response.err_msg);
+                }
 
-                    text = response + "";
-                },
-                error : function(jqXHR, textStatus, errorThrown) {
-                    errorModal.show("Error Getting Event Description", errorThrown);
-                },
-                async: false
-            });
+                descriptions[eventName] = response;
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                errorModal.show("Error Getting Event Description", errorThrown);
+            },
+            async: false
+        });
 
-        console.log("returning text: " + text);
-        return text;
+    console.log("Returning text: " + descriptions[eventName]);
+    return descriptions[eventName];
 }
