9f65cfe5f8f3fc8fc0985d46f0f5bb0cac474938
Aaron Chan <aarchan108@gmail.com>
2022-06-18 17:17:09 -0400

    Added new dropdown option for event definitions

--- src/main/javascript/event_definitions.js
+++ src/main/javascript/event_definitions.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {EventDefinitionCard} from "./event_definition";
 import GetDescription from "./get_description";
+import ReactDOM from "react-dom";
+import {Paginator} from "./paginator_component";
 
 class EventDefinitionsPage extends React.Component {
     constructor(props) {
@@ -10,6 +12,29 @@ class EventDefinitionsPage extends React.Component {
         for (let i = 0; i < eventNames.length; i++) {
             events[eventNames[i]] = GetDescription(eventNames[i]);
         }
+    }
+
+
+
+    render() {
+        let textClasses = "p-1 mr-1 card bg-light";
+        let events = [];
+        let descriptions = [];
+
+        for (let key in this.props.events) {
+            events.push(key);
+            descriptions.push(this.props.events[key]);
+        }
+
+
+        return (
+            <div className="m-1">
+                <div className="d-flex flex-row">
+                    <div className={textClasses} {events}></div>
+                    <div className={textClasses} {descriptions}></div>
+                </div>
+            </div>
+        )
     }
 }
 
