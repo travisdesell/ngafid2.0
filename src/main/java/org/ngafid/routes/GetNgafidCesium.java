acf900f86f778f940bc1cdf0e99fc576b8639cf9
Aaron <30483634+AarC10@users.noreply.github.com>
2022-06-21 09:15:01 -0600

    Added Ability to Replay Flights in Cesium (#28)
    * Add null checks for rpm in NGAFID Cesium route since Parrots dont provide that data
    
    * Add airplane model to phases
    
    * Get date and time of flights in cesium route for clock initalization in Cesium
    
    * Modify cesium response to include new times and remove redundant ArrayList inits
    
    * Include totalTime and timeStepInSeconds in cesium response
    
    * Fixed variables for timestep and total seconds
    
    * Add start and end times to Cesium response
    
    * Set Cesium clock to correct flight date
    
    * Modified cesium viewer settings. Added a tracked entity to be shown
    
    * Tracking works. Timing might need to be adjusted
    
    * Added airplane model
    
    * Add button to lock camera to airframe or free cam
    
    * Make initializing replays a function to allow for multiple flights in the future
    
    * Update CesiumResponse to check for airframe type too
    
    * Implement logic for changing model to load
    
    * Fix flight replay only going halfway
    
    * Remove timesteps variable
    
    * Add drone model support
    
    * Remove timestep variables from js script
    
    * Remove timestep fields from CesiumResponse class
    
    * Uncomment viewModel in cesium template to stop js problems
    
    * Make all flights show up as red dots for now until duplicating models is figured out
    
    * Randomize color of flight paths. Need to experiment with different Cesium materials
    
    * More visible paths added
    
    * Modify toggle button to shift between tracked airframes
    
    * Added print statement for locking
    
    * Attempt at improving airframeModels dictionary. Still not showing plane models
    
    * Get model to be cloned and not have Cesium break but models still not showing
    
    * Set cesium seed to use darker colors
    
    * Figured out cesium hasnt retrieved models before loading flight
    
    * Planes now show
    
    * Lower red dot tracking size to only show model if not null
    
    * Rearrange code blocks for better readability
    
    * Throw default phase into its own function
    
    * Fix viewing on startup
    
    * Add camera icon for lock button
    
    * HTTP req for cesium page
    
    * Rename globeClicked to mapClicked in flight component
    
    * Replay button works to open cesium. Need to fix pushing IDs to new selected id arr
    
    * Fix storing flight IDs
    
    * Shows globe as toggled when ID is selected for replay
    
    * Fix drone model not being loaded in
    
    Co-authored-by: travisdesell <travis.desell@gmail.com>

--- src/main/java/org/ngafid/routes/GetNgafidCesium.java
+++ src/main/java/org/ngafid/routes/GetNgafidCesium.java
@@ -1,7 +1,6 @@
 package org.ngafid.routes;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -20,6 +19,8 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 
+import org.ngafid.common.TimeUtils;
+import org.ngafid.flights.StringTimeSeries;
 import spark.Route;
 import spark.Request;
 import spark.Response;
@@ -32,10 +33,7 @@ import org.ngafid.accounts.User;
 import org.ngafid.flights.Flight;
 import org.ngafid.flights.DoubleTimeSeries;
 
-import java.util.HashSet;
 import java.util.*;
-
-import org.ngafid.filters.Filter;
 
 public class GetNgafidCesium implements Route {
     private static final Logger LOG = Logger.getLogger(GetNgafidCesium.class.getName());
@@ -50,12 +48,38 @@ public class GetNgafidCesium implements Route {
         ArrayList<Double> flightGeoAglCruise;
         ArrayList<Double> flightGeoInfoAgl;
 
-        public CesiumResponse(ArrayList<Double> flightGeoAglTaxiing, ArrayList<Double> flightGeoAglTakeOff, ArrayList<Double> flightGeoAglClimb, ArrayList<Double> flightGeoAglCruise, ArrayList<Double> flightGeoInfoAgl) {
+
+        ArrayList<String> flightTaxiingTimes;
+        ArrayList<String> flightTakeOffTimes;
+        ArrayList<String> flightClimbTimes;
+        ArrayList<String> flightCruiseTimes;
+        ArrayList<String> flightAglTimes;
+        String startTime;
+        String endTime;
+        String airframeType;
+
+        public CesiumResponse(ArrayList<Double> flightGeoAglTaxiing, ArrayList<Double> flightGeoAglTakeOff,
+                              ArrayList<Double> flightGeoAglClimb, ArrayList<Double> flightGeoAglCruise,
+                              ArrayList<Double> flightGeoInfoAgl, ArrayList<String> flightTaxiingTimes,
+                              ArrayList<String> flightTakeOffTimes, ArrayList<String> flightClimbTimes,
+                              ArrayList<String> flightCruiseTimes, ArrayList<String> flightAglTimes,
+                              String airframeType) {
+
             this.flightGeoAglTaxiing = flightGeoAglTaxiing;
             this.flightGeoAglTakeOff = flightGeoAglTakeOff;
             this.flightGeoAglClimb = flightGeoAglClimb;
             this.flightGeoAglCruise = flightGeoAglCruise;
             this.flightGeoInfoAgl = flightGeoInfoAgl;
+
+            this.flightTaxiingTimes = flightTaxiingTimes;
+            this.flightTakeOffTimes = flightTakeOffTimes;
+            this.flightClimbTimes = flightClimbTimes;
+            this.flightCruiseTimes = flightCruiseTimes;
+            this.flightAglTimes = flightAglTimes;
+
+            this.startTime = flightAglTimes.get(0);
+            this.endTime = flightAglTimes.get(flightAglTimes.size() - 1);
+            this.airframeType = airframeType;
         }
     }
 
@@ -111,9 +135,10 @@ public class GetNgafidCesium implements Route {
             Map<String, Object> flights = new HashMap<String, Object>();
 
             for (String flightIdNew : flightIdsAll) {
-
                 Flight incomingFlight = Flight.getFlight(Database.getConnection(), Integer.parseInt(flightIdNew));
                 int flightIdNewInteger = Integer.parseInt(flightIdNew);
+
+                String airframeType = incomingFlight.getAirframeType();
 
                 DoubleTimeSeries altMsl = DoubleTimeSeries.getDoubleTimeSeries(connection, flightIdNewInteger, "AltMSL");
                 DoubleTimeSeries latitude = DoubleTimeSeries.getDoubleTimeSeries(connection, flightIdNewInteger, "Latitude");
@@ -122,11 +147,20 @@ public class GetNgafidCesium implements Route {
                 DoubleTimeSeries rpm = DoubleTimeSeries.getDoubleTimeSeries(connection, flightIdNewInteger, "E1 RPM");
                 DoubleTimeSeries groundSpeed = DoubleTimeSeries.getDoubleTimeSeries(connection, flightIdNewInteger, "GndSpd");
 
+                StringTimeSeries date = StringTimeSeries.getStringTimeSeries(connection, flightIdNewInteger, "Lcl Date");
+                StringTimeSeries time = StringTimeSeries.getStringTimeSeries(connection, flightIdNewInteger, "Lcl Time");
+
                 ArrayList<Double> flightGeoAglTaxiing = new ArrayList<>();
                 ArrayList<Double> flightGeoAglTakeOff = new ArrayList<>();
                 ArrayList<Double> flightGeoAglClimb = new ArrayList<>();
                 ArrayList<Double> flightGeoAglCruise = new ArrayList<>();
                 ArrayList<Double> flightGeoInfoAgl = new ArrayList<>();
+
+                ArrayList<String> flightTaxiingTimes = new ArrayList<>();
+                ArrayList<String> flightTakeOffTimes = new ArrayList<>();
+                ArrayList<String> flightClimbTimes = new ArrayList<>();
+                ArrayList<String> flightCruiseTimes = new ArrayList<>();
+                ArrayList<String> flightAglTimes = new ArrayList<>();
 
                 int initCounter = 0;
                 int takeoffCounter = 0;
@@ -142,6 +176,7 @@ public class GetNgafidCesium implements Route {
                         flightGeoAglTaxiing.add(longitude.get(i));
                         flightGeoAglTaxiing.add(latitude.get(i));
                         flightGeoAglTaxiing.add(altAgl.get(i));
+                        flightTaxiingTimes.add(date.get(i) + "T" + time.get(i) + "Z");
 
                         if ((rpm != null && rpm.get(i) >= 2100) && groundSpeed.get(i) > 14.5 && groundSpeed.get(i) < 80) {
                             break;
@@ -159,6 +194,8 @@ public class GetNgafidCesium implements Route {
                                 flightGeoAglTakeOff.add(longitude.get(i));
                                 flightGeoAglTakeOff.add(latitude.get(i));
                                 flightGeoAglTakeOff.add(altAgl.get(i));
+                                flightTakeOffTimes.add(date.get(i) + "T" + time.get(i) + "Z");
+
                                 initCounter++;
                             } else if (takeoffCounter > 15) {
                                 break;
@@ -180,6 +217,8 @@ public class GetNgafidCesium implements Route {
                                 flightGeoAglClimb.add(longitude.get(i));
                                 flightGeoAglClimb.add(latitude.get(i));
                                 flightGeoAglClimb.add(altAgl.get(i));
+                                flightClimbTimes.add(date.get(i) + "T" + time.get(i) + "Z");
+
                                 initCounter++;
                             }
                             if (altAgl.get(i) >= 500) {
@@ -201,6 +240,7 @@ public class GetNgafidCesium implements Route {
                             flightGeoAglCruise.add(longitude.get(i));
                             flightGeoAglCruise.add(latitude.get(i));
                             flightGeoAglCruise.add(altAgl.get(i));
+                            flightCruiseTimes.add(date.get(i) + "T" + time.get(i) + "Z");
                         }
                         countPostCruise++;
                     }
@@ -213,6 +253,7 @@ public class GetNgafidCesium implements Route {
                         flightGeoInfoAgl.add(longitude.get(i));
                         flightGeoInfoAgl.add(latitude.get(i));
                         flightGeoInfoAgl.add(altAgl.get(i));
+                        flightAglTimes.add(date.get(i) + "T" + time.get(i) + "Z");
                     }
                 }
 
@@ -221,7 +262,7 @@ public class GetNgafidCesium implements Route {
                     Spark.halt(401, "User did not have access to this flight.");
                 }
 
-                CesiumResponse cr = new CesiumResponse(flightGeoAglTaxiing, flightGeoAglTakeOff, flightGeoAglClimb, flightGeoAglCruise, flightGeoInfoAgl);
+                CesiumResponse cr = new CesiumResponse(flightGeoAglTaxiing, flightGeoAglTakeOff, flightGeoAglClimb, flightGeoAglCruise, flightGeoInfoAgl, flightTaxiingTimes, flightTakeOffTimes, flightClimbTimes, flightCruiseTimes, flightAglTimes, airframeType);
 
                 flights.put(flightIdNew, cr);
 
