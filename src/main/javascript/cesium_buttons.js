c3f02709c4199b3e5fa76d5e461f36d046da32c6
Aaron Chan <aarchan108@gmail.com>
2022-07-27 22:33:34 -0400

    Update State. Trying to figure out how to disable buttons

--- src/main/javascript/cesium_buttons.js
+++ src/main/javascript/cesium_buttons.js
@@ -6,6 +6,10 @@ class CesiumButtons extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            disabled: cesiumFlightsSelected.length <= 0
+        };
+
 
     }
 
@@ -15,7 +19,7 @@ class CesiumButtons extends React.Component {
     viewCesiumFlights() {
         let URL = "/protected/ngafid_cesium?flight_id=";
 
-        if (cesiumFlightsSelected.length > 0 ){
+        if (cesiumFlightsSelected.length > 0) {
             URL += cesiumFlightsSelected.join("&flight_id=");
         } else {
             return;
@@ -27,29 +31,43 @@ class CesiumButtons extends React.Component {
     /**
      * Handles clearing all selected flights for multiple flight replays
      */
-     clearCesiumFlights() {
-         cesiumFlightsSelected.forEach((removedFlight) => {
-             console.log("Removed " + removedFlight);
-             let toggleButton = document.getElementById("cesiumToggled" + removedFlight);
-             toggleButton.click();
-         });
+    clearCesiumFlights() {
+        cesiumFlightsSelected.forEach((removedFlight) => {
+            console.log("Removed " + removedFlight);
+            let toggleButton = document.getElementById("cesiumToggled" + removedFlight);
+            toggleButton.click();
+        });
 
-         if (cesiumFlightsSelected.length > 0) {
-             this.clearCesiumFlights();
-         }
+        if (cesiumFlightsSelected.length > 0) {
+            this.clearCesiumFlights();
+        }
+
+        this.setState({
+            disabled: true
+        })
+    }
+
+    updateState() {
+        this.setState({
+            disabled: cesiumFlightsSelected.length <= 0
+        })
     }
 
     render() {
+        console.log("Disabled state: " + this.state.disabled);
+        console.log("Arr status: " + cesiumFlightsSelected.length + " " + cesiumFlightsSelected.length > 0);
         return (
             <div className="col form-row input-group m-0 p-0">
                 <div className="input-group-prepend p-0">
-                        <button className="btn btn-sm btn-primary" onClick={() => this.viewCesiumFlights()}>
-                            View Selected Replays
-                        </button>
+                    <button className="btn btn-sm btn-primary" disabled={this.state.disabled}
+                            onClick={() => this.viewCesiumFlights()}>
+                        View Selected Replays
+                    </button>
 
-                        <button className="btn btn-sm btn-primary" onClick={() => this.clearCesiumFlights()}>
-                            Clear Selected Replays
-                        </button>
+                    <button className="btn btn-sm btn-primary" disabled={this.state.disabled}
+                            onClick={() => this.clearCesiumFlights()}>
+                        Clear Selected Replays
+                    </button>
                 </div>
             </div>
         )
