02efa7a790c8dc72f1cebd28d808c959c0391b36
Aaron Chan <aarchan108@gmail.com>
2022-07-30 12:32:19 -0400

    Enqueue Dequeue methods for TSQ

--- src/main/java/org/ngafid/common/TimeSeriesQueue.java
+++ src/main/java/org/ngafid/common/TimeSeriesQueue.java
@@ -1,9 +1,5 @@
 package org.ngafid.common;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
-
 public class TimeSeriesQueue<ValueType> {
     private TimeSeriesNode<ValueType> front;
     private TimeSeriesNode<ValueType> back;
@@ -14,6 +10,35 @@ public class TimeSeriesQueue<ValueType> {
         this.front = null;
         this.back = null;
         this.size = 0;
+    }
+
+    public void enqueue(double time, ValueType value) {
+        this.enqueue(new TimeSeriesNode<>(time, value));
+    }
+
+    public void enqueue(TimeSeriesNode<ValueType> node) {
+        if (this.front == null) {
+            this.front = node;
+            this.back = node;
+        } else {
+            this.back.setNext(node);
+            this.back = back.getNext();
+        }
+
+        size++;
+    }
+
+    public ValueType dequeue() {
+        ValueType popped = this.front.getValue();
+
+        this.front = this.front.getNext();
+
+        if (--size == 0) {
+            this.front = null;
+            this.back = null;
+        }
+
+        return popped;
     }
 
     public TimeSeriesNode<ValueType> getFront() {
