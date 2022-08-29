a96a357bcd408d301be3dc8718544a6964c502d5
Aaron Chan <aarchan108@gmail.com>
2022-08-28 22:05:22 -0400

    Clean up attr value pair file

--- src/main/java/org/ngafid/flights/dji/AttrValuePair.java
+++ src/main/java/org/ngafid/flights/dji/AttrValuePair.java
@@ -1,25 +1,20 @@
 package org.ngafid.flights.dji;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 public class AttrValuePair {
-    private String attr;
+    private final String attr;
 
     private String value;
 
     public AttrValuePair(String attr, String value) {
         this.attr = attr;
         StringBuilder cleanValue = new StringBuilder();
-        try {
-            byte dirtyBytes[] = value.getBytes("UTF-8");
-            for (int i = 0; i < dirtyBytes.length; i++) {
-                byte b = dirtyBytes[i];
-                if (0x20 <= b && b <= 0x7E && b != 0x2C) {
-                    cleanValue.append(new String(new byte[]{b}));
-                }
+        byte[] dirtyBytes = value.getBytes(StandardCharsets.UTF_8);
+        for (byte b : dirtyBytes) {
+            if (0x20 <= b && b <= 0x7E && b != 0x2C) {
+                cleanValue.append(new String(new byte[]{b}));
             }
-        } catch (UnsupportedEncodingException e) {
-            // just ignore any errors
         }
         this.value = cleanValue.toString();
     }
