ec3b7d0a056738af15fdc352e2df85828f529265
aidanlabella <apl1341@rit.edu>
2022-09-13 14:04:15 -0400

    fixed bug where a NullPtrEx is thrown by removing a debug stmt

--- src/main/java/org/ngafid/flights/Flight.java
+++ src/main/java/org/ngafid/flights/Flight.java
@@ -3319,10 +3319,11 @@ public class Flight {
                 int flightId = resultSet.getInt(1);
                 this.id = flightId;
 
-                for (String key : doubleTimeSeries.keySet()) {
-                    System.out.println("double time series key: '" + key);
-                    System.out.println("\tis " + doubleTimeSeries.get(key).toString());
-                }
+                // Comment this out unless debugging
+                //for (String key : doubleTimeSeries.keySet()) {
+                    //System.out.println("double time series key: '" + key);
+                    //System.out.println("\tis " + doubleTimeSeries.get(key).toString());
+                //}
 
                 for (DoubleTimeSeries series : doubleTimeSeries.values()) {
                     System.out.println(series);
