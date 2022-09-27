1640cab5291e3d6fb7440841c8db9616e5cc4d85
Aaron Chan <aarchan108@gmail.com>
2022-09-27 14:19:49 -0400

    Stub out methods for interfacing with DatCon jar

---
+++ src/main/java/org/ngafid/flights/dji/DatConExecutor.java
@@ -0,0 +1,18 @@
+package org.ngafid.flights.dji;
+
+import java.io.File;
+
+public class DatConExecutor {
+    public static void process(String filename) {
+        process(new File(filename));
+    }
+
+    public static void process(File file) {
+        return;
+    }
+
+    public static void processZip() {
+        return;
+    }
+
+}
