c12f98b85169933bf417e15fd9390302b8c54d9f
Aaron Chan <aarchan108@gmail.com>
2022-11-10 11:30:35 -0500

    create rows function wip

--- src/main/javascript/event_definitions_display.js
+++ src/main/javascript/event_definitions_display.js
@@ -8,25 +8,17 @@ class EventDefinition extends React.Component {
         super(props);
 
         this.eventName = props.eventName;
+        this.airframeName = props.airframeName;
         this.eventDef = props.eventDef;
     }
 
     render() {
-        const styleName = {flex: "0 0 11em"};
-        const styleDefinition = {};
-
-        let textClasses = "p-1 mr-1 card bg-light";
-        let eventNameText = this.eventName;
-        let eventDefText = this.eventDef;
-
         return (
-            <div className="m-1">
-                <div className="d-flex flex-row">
-                    <div className={textClasses} style={styleName}>{eventNameText}</div>
-                    <div className={textClasses + " flex-fill"} style={styleDefinition}>{eventDefText}</div>
-                </div>
+            <div>
+                <td>{this.eventName}</td>
+                <td>{this.airframeName}</td>
+                <td>{this.eventDef}</td>
             </div>
-
         )
     }
 }
@@ -48,39 +40,45 @@ class EventDefinitionsDisplayPage extends React.Component {
         // console.log(this.events);
     }
 
+    createRows() {
+        let rows = [];
+
+        for (let key in Object.keys(this.events)) {
+            for (let id in Object.keys(this.events[key])) {
+                console.log(this.events[key][id]);
+                rows.push(<EventDefinition eventName={this.events[key][id].name} airframeName={this.events[key][id].airframe} eventDef={this.events[key][id].description}/>);
+            }
+        }
+
+        return rows;
+
+    }
+
     render() {
+        let rows = this.createRows();
+        console.log(rows);
         return (
             <div>
                 <SignedInNavbar activePage="event definitions" waitingUserCount={waitingUserCount}
                                 fleetManager={fleetManager} unconfirmedTailsCount={unconfirmedTailsCount}
                                 modifyTailsAccess={modifyTailsAccess} plotMapHidden={plotMapHidden}/>
                 <div className="container-fluid" style={{backgroundColor: "white"}}>
-                     <div className="row">
-                         <div className="col-md-12">
-                             <table className="table table-striped table-bordered table-hover">
-                                 <thead>
-                                 <tr>
-                                     <th>Event Name</th>
-                                     <th>Aircraft Type</th>
-                                     <th>Event Definition</th>
-                                 </tr>
-                                 </thead>
-                                 <tbody>
-                                {Object.keys(this.events).map((eventKey) => {
-                                    Object.keys(this.events[eventKey]).map((airframeName) => {
-                                        return (
-                                            <tr>
-                                                <td>{eventKey}</td>
-                                                <td>{this.events[eventKey]}</td>
-                                                <td>{this.events[eventKey][airframeName]}</td>
-                                            </tr>
-                                        )
-                                    })
-                                })}
+                    <div className="row">
+                        <div className="col-md-12">
+                            <table className="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>Event Name</th>
+                                    <th>Aircraft Type</th>
+                                    <th>Event Definition</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.createRows()}
                                 </tbody>
-                             </table>
-                         </div>
-                     </div>
+                            </table>
+                        </div>
+                    </div>
                 </div>
 
             </div>
