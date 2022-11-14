85c2d43ca24ff055bcf33f10699f5bfbffed1f77
Aaron <30483634+AarC10@users.noreply.github.com>
2022-11-14 10:15:16 -0500

    Improved Event Definitions Page (#60)
    Updates the event definitions page to display airframe type and all event definitions.

---
+++ src/main/javascript/get_all_descriptions.js
@@ -0,0 +1,32 @@
+import {errorModal} from "./error_modal";
+
+
+export default function GetAllDescriptions() {
+    let descriptions = null;
+
+    console.log("Getting  all descriptions from the server.");
+    $.ajax({
+            type: 'GET',
+            url: '/protected/get_all_event_descriptions',
+            data : {},
+            dataType : 'json',
+            success : function(response) {
+                console.log("received response: " + response);
+
+                $('#loading').hide();
+
+                if (response.err_msg) {
+                    errorModal.show(response.err_title, response.err_msg);
+                }
+
+                descriptions = response;
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                errorModal.show("Error Getting Event Description", errorThrown);
+            },
+            async: false
+        });
+
+    console.log("Returning descriptions: " + descriptions);
+    return descriptions;
+}
