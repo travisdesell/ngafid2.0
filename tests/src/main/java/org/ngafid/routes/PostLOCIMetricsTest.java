7ab8b9050a9fe9906938ed0c58887d4af996cabd
Aidan LaBella <apl1341@rit.edu>
2022-11-14 10:18:40 -0500

    initial commit

---
+++ tests/src/main/java/org/ngafid/routes/PostLOCIMetricsTest.java
@@ -0,0 +1,13 @@
+package com.vogella.junit.first;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class PostLOCIMetricsTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
