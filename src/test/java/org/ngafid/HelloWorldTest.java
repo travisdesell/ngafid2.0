2030556a88562076e39ee6ae357b24bde36e25b6
Aaron Chan <aarchan108@gmail.com>
2023-03-21 13:44:27 -0400

    Undo broken test. Test runner should be working now

--- src/test/java/org/ngafid/HelloWorldTest.java
+++ src/test/java/org/ngafid/HelloWorldTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-        String helloWorld = "Hello ld!";
+        String helloWorld = "Hello World!";
         assertEquals("Hello World!", helloWorld);
     }
 }
