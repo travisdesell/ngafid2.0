e7d20e7e23a25333a81ca7ccbe991a05d8d22c82
Travis Desell <travis.desell@gmail.com>
2023-03-27 13:19:18 -0500

    Local fixes

--- src/main/java/org/ngafid/flights/Flight.java
+++ src/main/java/org/ngafid/flights/Flight.java
@@ -2218,13 +2218,12 @@ public class Flight {
             int rowCount = 0;
             LocalTime prevTimeStamp = null;
             int secondsDiffSum = 0;
-            for(int i = 0; i < lclTime.size(); i++){
-                if (!lclTime.get(i).isBlank()){
-                    if (prevTimeStamp == null){
+            for (int i = 0; i < lclTime.size(); i++) {
+                if (!lclTime.get(i).isBlank()) {
+                    if (prevTimeStamp == null) {
                         prevTimeStamp = LocalTime.parse(lclTime.get(i));
                         rowCount++;
-                    }
-                    else {
+                    } else {
                         LocalTime currTimeStamp = LocalTime.parse(lclTime.get(i));
                         if (!currTimeStamp.equals(prevTimeStamp)) {
                             secondsDiffSum = secondsDiffSum + (int)SECONDS.between(prevTimeStamp,currTimeStamp);
@@ -2234,7 +2233,7 @@ public class Flight {
                     }
                 }
             }
-            if ((double)secondsDiffSum/rowCount > 1){
+            if ((double)secondsDiffSum/(double)rowCount > 1.1) {
                 exceptions.add(new MalformedFlightFileException("Time series have frequency greater than 1Hz"));
             }
         }
