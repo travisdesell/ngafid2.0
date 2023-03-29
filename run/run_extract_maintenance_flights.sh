969a59b6e1c9aa894cc0ff7af5b4a2cf3a130819
Joshua Karns <jkarns275@gmail.com>
2023-03-29 13:59:21 -0400

    Merge documentation folders

--- run/run_extract_maintenance_flights.sh
+++
@@ -1,7 +0,0 @@
-#sh run_extract_maintenance_flights.sh -f 10 -o ./test_flight
-
-ARGUMENTS="$@"
-
-echo "command line args are: '$ARGUMENTS'"
-
-mvn compile exec:java -Dexec.mainClass="org.ngafid.ExtractMaintenanceFlights" -Dexec.args="$ARGUMENTS"
