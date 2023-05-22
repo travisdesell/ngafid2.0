98e356ab996c1dab60ed2ef68af60d36f4ace2fc
mayurjs26 <mayurjs26@gmail.com>
2023-05-22 16:38:10 -0400

    1hz warning fix for Airsync uploads

--- src/main/java/org/ngafid/flights/Flight.java
+++ src/main/java/org/ngafid/flights/Flight.java
@@ -2217,32 +2217,32 @@ public class Flight {
             exceptions.add(e);
         }
 
-        // Checking whether the timeseries contains data with frequency greater than 1Hz
-        //StringTimeSeries lclTime = this.getStringTimeSeries(LCL_TIME);
-        //if (lclTime != null){
-            //int rowCount = 0;
-            //LocalTime prevTimeStamp = null;
-            //int secondsDiffSum = 0;
-            //for(int i = 0; i < lclTime.size(); i++){
-                //if (!lclTime.get(i).isBlank()){
-                    //if (prevTimeStamp == null){
-                        //prevTimeStamp = LocalTime.parse(lclTime.get(i));
-                        //rowCount++;
-                    //}
-                    //else {
-                        //LocalTime currTimeStamp = LocalTime.parse(lclTime.get(i));
-                        //if (!currTimeStamp.equals(prevTimeStamp)) {
-                            //secondsDiffSum = secondsDiffSum + (int)SECONDS.between(prevTimeStamp,currTimeStamp);
-                            //rowCount++;
-                        //}
-                        //prevTimeStamp = currTimeStamp;
-                    //}
-                //}
-            //}
-            //if ((double)secondsDiffSum/rowCount > 1){
-                //exceptions.add(new MalformedFlightFileException("Time series have frequency greater than 1Hz"));
-            //}
-        //}
+//         Checking whether the timeseries contains data with frequency greater than 1Hz
+        StringTimeSeries lclTime = this.getStringTimeSeries(LCL_TIME);
+        if (lclTime != null){
+            int rowCount = 0;
+            LocalTime prevTimeStamp = null;
+            int secondsDiffSum = 0;
+            for(int i = 0; i < lclTime.size(); i++){
+                if (!lclTime.get(i).isBlank()){
+                    if (prevTimeStamp == null){
+                        prevTimeStamp = LocalTime.parse(lclTime.get(i));
+                        rowCount++;
+                    }
+                    else {
+                        LocalTime currTimeStamp = LocalTime.parse(lclTime.get(i));
+                        if (!currTimeStamp.equals(prevTimeStamp)) {
+                            secondsDiffSum = secondsDiffSum + (int)SECONDS.between(prevTimeStamp,currTimeStamp);
+                            rowCount++;
+                        }
+                        prevTimeStamp = currTimeStamp;
+                    }
+                }
+            }
+            if ((double)secondsDiffSum/rowCount > 1.1){
+                exceptions.add(new MalformedFlightFileException("Time series have frequency greater than 1Hz"));
+            }
+        }
 
         try {
             if (!airframeName.equals("ScanEagle") && hasCoords && hasAGL) {
