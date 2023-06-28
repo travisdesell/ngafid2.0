87c1d03d3e56da5df0514cb14ce36979c0e7819f
mayurjs26 <mayurjs26@gmail.com>
2023-06-27 22:39:44 -0400

    fixed create_tables.php

--- db/create_tables.php
+++ db/create_tables.php
@@ -7,7 +7,7 @@ $cwd[__FILE__] = dirname($cwd[__FILE__]);
 require_once($cwd[__FILE__] . "/my_query.php");
 
 $drop_tables = false;
-$update_2022_02_17 = true;
+$update_2022_02_17 = false;
 $update_turn_to_final = false;
 $update_visited_airports = false;
 $update_uploads_for_raise = false;
