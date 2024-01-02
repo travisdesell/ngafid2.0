29b89bd37bf506abae90ea4da12e2a14500b3527
Aaron <30483634+AarC10@users.noreply.github.com>
2024-01-02 14:14:56 -0500

    Update Event Definition Page (#128)
    Adds the ability to edit existing event definitions on the create event definition page.

---
+++ src/main/java/org/ngafid/routes/GetEventManager.java
@@ -0,0 +1,106 @@
+package org.ngafid.routes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.HashMap;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
+import spark.Route;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+import spark.Spark;
+
+
+
+import org.ngafid.Database;
+import org.ngafid.WebServer;
+import org.ngafid.accounts.User;
+import org.ngafid.flights.Airframes;
+import org.ngafid.flights.DoubleTimeSeries;
+import org.ngafid.flights.Upload;
+import org.ngafid.flights.Itinerary;
+import org.ngafid.flights.Tails;
+import org.ngafid.events.EventDefinition;
+
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+
+public class GetEventManager implements Route {
+    private static final Logger LOG = Logger.getLogger(GetEventManager.class.getName());
+    private Gson gson;
+
+    public GetEventManager(Gson gson) {
+        this.gson = gson;
+
+        LOG.info("post " + this.getClass().getName() + " initalized");
+    }
+
+    public GetEventManager(Gson gson, String messageType, String messageText) {
+        this.gson = gson;
+
+        LOG.info("post " + this.getClass().getName() + " initialized");
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.info("handling " + this.getClass().getName() + " route");
+
+        String resultString = "";
+        String templateFile = WebServer.MUSTACHE_TEMPLATE_DIR + "manage_events.html";
+        LOG.severe("template file: '" + templateFile + "'");
+
+        try  {
+            MustacheFactory mf = new DefaultMustacheFactory();
+            Mustache mustache = mf.compile(templateFile);
+
+            HashMap<String, Object> scopes = new HashMap<String, Object>();
+
+            scopes.put("navbar_js", Navbar.getJavascript(request));
+
+            final Session session = request.session();
+            User user = session.attribute("user");
+            int fleetId = user.getFleetId();
+
+            if (!user.isAdmin()) {
+                LOG.severe("INVALID ACCESS: user does not have access to this page.");
+                Spark.halt(401, "User did not have access to this page.");
+                return null;
+            }
+
+            Connection connection = Database.getConnection();
+
+            scopes.put("event_manager_js",
+                    "var eventDefinitions = JSON.parse('" + gson.toJson(EventDefinition.getAll(connection)) + "');\n" +
+                    "var airframes = JSON.parse('" + gson.toJson(Airframes.getAll(connection)) + "');\n" +
+                    "var doubleTimeSeriesNames = JSON.parse('" + gson.toJson(DoubleTimeSeries.getAllNames(connection, fleetId)) + "');\n" +
+                    "var airframeMap = JSON.parse('" + gson.toJson(Airframes.getIdToNameMap(connection)) + "');\n"
+                    );
+
+            StringWriter stringOut = new StringWriter();
+            mustache.execute(new PrintWriter(stringOut), scopes).flush();
+            resultString = stringOut.toString();
+        } catch (Exception e) {
+            LOG.severe(e.toString());
+            return gson.toJson(new ErrorResponse(e));
+
+        }
+
+        return resultString;
+    }
+}
