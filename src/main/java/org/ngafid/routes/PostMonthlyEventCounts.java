fecb61564464b001c6bd60445ab84981893772c9
Josh Karns <jkarns275@gmail.com>
2024-06-04 16:27:19 -0400

    Fix trends and summary pages (#141)
    * Use a ThreadLocal database connection
    
    * Initial implementation of aggregate event fetching function
    
    * Change class name to match file
    
    * Now separate by airframe id
    
    * Add toString method to AirframeEventCount
    
    * Add toString method to AirframeEventCount
    
    * Query now grabs flight count as well
    
    * Group by fleet
    
    * Add total flight counting queries
    
    * Preliminary rewrite for fetching event statistics
    
    * Refactored a bit
    
    * Unify welcome and aggregate welcome page into summary page
    
    * Added Uploads section to Summary pages.
    
    * Fix rendering of durations
    
    * Update java version
    
    * Fix even count fetching
    
    * Separate requests for each statistic
    
    * Rework notifications section
    
    * Update github action for java version bump
    
    * Fixed some bugs

--- src/main/java/org/ngafid/routes/PostMonthlyEventCounts.java
+++ src/main/java/org/ngafid/routes/PostMonthlyEventCounts.java
@@ -42,6 +42,8 @@ public class PostMonthlyEventCounts implements Route {
         try {
             Connection connection = Database.getConnection();
             Map<String, EventStatistics.MonthlyEventCounts> eventCountsMap;
+            Map<String, Map<String, EventStatistics.MonthlyEventCounts>> map;
+
             if (aggregateTrendsPage) {
                 if (!user.hasAggregateView()) {
                     LOG.severe("INVALID ACCESS: user did not have aggregate access to view aggregate trends page.");
@@ -49,10 +51,9 @@ public class PostMonthlyEventCounts implements Route {
                     return null;
                 }
 
-                var map = EventStatistics.getMonthlyEventCounts(connection, -1, LocalDate.parse(startDate), LocalDate.parse(endDate));
-                eventCountsMap = map.get(eventName);
-            }
-            else {
+                map = EventStatistics.getMonthlyEventCounts(connection, -1, LocalDate.parse(startDate), LocalDate.parse(endDate));
+            } else {
+
                 int fleetId = user.getFleetId();
                 //check to see if the user has upload access for this fleet.
                 if (!user.hasViewAccess(fleetId)) {
@@ -60,14 +61,15 @@ public class PostMonthlyEventCounts implements Route {
                     Spark.halt(401, "User did not have access to view imports for this fleet.");
                     return null;
                 }
-
-                var map = EventStatistics.getMonthlyEventCounts(connection, fleetId, LocalDate.parse(startDate), LocalDate.parse(endDate));
-                eventCountsMap = map.get(eventName);
+              
+                map = EventStatistics.getMonthlyEventCounts(connection, fleetId, LocalDate.parse(startDate), LocalDate.parse(endDate));
             }
 
-//            LOG.severe("\n\n" + gson.toJson(eventCountsMap) + "\n\n");
-
-            return gson.toJson(eventCountsMap);
+            if (eventName == null) {
+                return gson.toJson(map);
+            } else {
+                return gson.toJson(map.get(eventName));
+            }
         } catch (SQLException e) {
             return gson.toJson(new ErrorResponse(e));
         }
