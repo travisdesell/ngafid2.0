ad6fc1d8f9085e201ead62f966efa6af27021ef3
Aidan Cusack <aidanqsack@gmail.com>
2024-08-12 16:38:54 -0400

    Paginator changes, Flight upload 'triggerInput' method moved to Paginator

--- src/main/javascript/paginator_component.js
+++ src/main/javascript/paginator_component.js
@@ -47,6 +47,10 @@ class Paginator extends React.Component {
     jumpPage(page) {
         if (page < this.props.numberPages && page >= 0){
             this.props.updateCurrentPage(page);
+
+            if (typeof this.props.items === 'undefined')
+                return;
+
             this.props.submitFilter();
         }
     }
@@ -56,6 +60,10 @@ class Paginator extends React.Component {
      *        */
     nextPage() {
         this.props.updateCurrentPage(this.props.currentPage + 1);
+
+        if (typeof this.props.items === 'undefined')
+            return;
+
         this.props.submitFilter();
     }
 
@@ -64,6 +72,10 @@ class Paginator extends React.Component {
      **/
     previousPage() {
         this.props.updateCurrentPage(this.props.currentPage - 1);
+
+        if (typeof this.props.items === 'undefined')
+            return;
+
         this.props.submitFilter();
     }
 
@@ -71,9 +83,43 @@ class Paginator extends React.Component {
      ** Repaginates the page configuration when the numPerPage field has been changed by the user
      **/
     repaginate(pageSize) {
+
         console.log("Re-Paginating");
         this.props.updateItemsPerPage(pageSize);
+
+        if (typeof this.props.items === 'undefined')
+            return;
+
         this.props.submitFilter(true);
+    }
+
+    triggerInput() {
+        console.log("input triggered!");
+
+        let uploadsPageRef = this.props.uploadsPage;
+
+        $('#upload-file-input').trigger('click');
+
+        $('#upload-file-input:not(.bound)').addClass('bound').change(function() {
+            console.log("number files selected: " + this.files.length);
+            console.log( this.files );
+
+            if (this.files.length > 0) {
+                var file = this.files[0];
+                var filename = file.webkitRelativePath || file.fileName || file.name;
+
+                const isZip = file['type'].includes("zip");
+                console.log("isZip: " + isZip);
+
+                if (!filename.match(/^[a-zA-Z0-9_.-]*$/)) {
+                    errorModal.show("Malformed Filename", "The filename was malformed. Filenames must only contain letters, numbers, dashes ('-'), underscores ('_') and periods.");
+                } else if (!isZip) {
+                    errorModal.show("Malformed Filename", "Uploaded files must be zip files. The zip file should contain directories which contain flight logs (csv files). The directories should be named for the tail number of the airfraft that generated the flight logs within them.");
+                } else {
+                    uploadsPageRef.addUpload(file);
+                }
+            }
+        });
     }
 
     render() {
@@ -139,65 +185,93 @@ class Paginator extends React.Component {
             numTotalPages = 1;
         }
 
+        let PAGE_FLIGHTS = (window.location.pathname === "/protected/flights");
 
-        if (typeof this.props.items != 'undefined') {
-            return (
-                <div className="card border-secondary"
-                    style={{
-                        backgroundColor:"var(--c_bg)",
-                        textAlign: "center",
-                        width: "100%"
-                    }}
-                >
-                    <div className="row m-0 p-2">
-                        <button className="btn btn-sm btn-info mr-2" disabled>Page: {this.props.currentPage + 1} of {numTotalPages}</button>
+        return (
+            <div className="card border-secondary"
+                style={{
+                    backgroundColor:"var(--c_bg)",
+                    textAlign: "center",
+                    width: "100%",
+                    userSelect: "none",
+                }}
+            >
 
-                        <Pagination size="sm" className="m-0 mr-2">
-                            <Pagination.First disabled={this.props.currentPage === 0} onClick={() => this.jumpPage(0)}/>
-                            <Pagination.Prev disabled={this.props.currentPage === 0} onClick={() => this.previousPage()}/>
+                <div className="row m-0 p-2 d-flex flex-column">
+                    
+                    <div className="d-flex flex-row flex-wrap" style={{rowGap:"0.50em"}}>
 
-                            {pages}
+                        <div className="row" style={{alignContent:"start", marginLeft:"0em", marginRight:"auto"}}>
 
-                            <Pagination.Next disabled={this.props.currentPage === this.props.numberPages - 1} onClick={() => this.nextPage()} />
-                            <Pagination.Last disabled={this.props.currentPage === this.props.numberPages - 1} onClick={() => this.jumpPage(this.props.numberPages - 1)} />
-                        </Pagination>
+                            <button className="btn btn-sm btn-info mr-2" disabled>
+                                Page: {this.props.currentPage + 1} of {numTotalPages}
+                            </button>
 
-                        <div className="col form-row input-group m-0 p-0">
-                            <div className="input-group-prepend p-0">
-                                <button className="btn btn-sm btn-primary" disabled={!this.state.goto_active} onClick={() => this.jumpPage(this.state.goto_value - 1)}>Go To</button>
+                            <Pagination size="sm" className="m-0 mr-2">
+                                <Pagination.First disabled={this.props.currentPage === 0} onClick={() => this.jumpPage(0)}/>
+                                <Pagination.Prev disabled={this.props.currentPage === 0} onClick={() => this.previousPage()}/>
+
+                                {pages}
+
+                                <Pagination.Next disabled={this.props.currentPage >= (this.props.numberPages-1)} onClick={() => this.nextPage()} />
+                                <Pagination.Last disabled={this.props.currentPage >= (this.props.numberPages-1)} onClick={() => this.jumpPage(this.props.numberPages - 1)} />
+                            </Pagination>
+
+                            <div className="col form-row input-group m-0 p-0" style={{width:"160px"}}>
+                                <div className="input-group-prepend p-0" style={{display:"flex", flexFlow:"column wrap", minWidth:"35%", maxWidth:"35%"}}>
+                                    <button className="btn btn-sm btn-primary" disabled={!this.state.goto_active} onClick={() => this.jumpPage(this.state.goto_value - 1)}>
+                                        Go To
+                                    </button>
+                                </div>
+                                <input id="jump-text" type="text" className="form-control col-2" placeholder="Page" style={{height:"31px", minWidth:"65%", maxWidth:"65%"}} onChange={(event) => {this.updateGoto(event);}}></input>
                             </div>
-                            <input id="jump-text" type="text" className="form-control col-2" placeholder="Page" style={{height:"31px"}} onChange={(event) => {this.updateGoto(event);}}></input>
                         </div>
 
-                        {(window.location.pathname === "/protected/flights") &&
-                            <CesiumButtons location={this.props.location}></CesiumButtons>
-                        }
+                        {/* RIGHT ELEMENTS */}
+                        <div className={"d-flex flex-row "+(PAGE_FLIGHTS?"":"ml-auto")} style={{justifyContent:"space-between", flexDirection:"row", alignContent:"end"}}>
+
+                            {
+                                //Show Upload Flights button on the Uploads page
+                                (window.location.pathname === "/protected/uploads") &&
+                                <button id="upload-flights-button" className="btn btn-primary btn-sm float-right mr-2" onClick={() => this.triggerInput()}>
+                                    <i className="fa fa-upload"></i> Upload Flights
+                                </button>
+                            }
+
+                            <div className="d-flex flex-row ml-auto"> 
+                                
+                                {sorter}
+
+                                <DropdownButton id="dropdown-item-button-resize" title={
+                                    <span style={{ overflow: "hidden", maxWidth: "8vw", textOverflow: "ellipsis", whiteSpace: "nowrap", display: "inline-block", verticalAlign: "-25%"}}>
+                                        {this.props.pageSize+ " " + this.props.itemName + " per page"}
+                                    </span>
+                                    } size="sm">
+                                    <Dropdown.Item as="button" onClick={() => this.repaginate(10)}>10 {this.props.itemName} per page</Dropdown.Item>
+                                    <Dropdown.Item as="button" onClick={() => this.repaginate(15)}>15 {this.props.itemName} per page</Dropdown.Item>
+                                    <Dropdown.Item as="button" onClick={() => this.repaginate(25)}>25 {this.props.itemName} per page</Dropdown.Item>
+                                    <Dropdown.Item as="button" onClick={() => this.repaginate(50)}>50 {this.props.itemName} per page</Dropdown.Item>
+                                    <Dropdown.Item as="button" onClick={() => this.repaginate(100)}>100 {this.props.itemName} per page</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
 
 
-                        {sorter}
+                            {
+                            //Show Cesium buttons on the Flights page
+                            (PAGE_FLIGHTS) &&
+                            <div className="ml-4">
+                                <CesiumButtons location={this.props.location}/>
+                            </div>
+                            }
 
-                        {
-                        (window.location.pathname === "/protected/uploads") &&
-                        <button id="upload-flights-button" className="btn btn-primary btn-sm float-right" onClick={() => this.triggerInput()}>
-                            <i className="fa fa-upload"></i> Upload Flights
-                        </button>
-                        }
-                        <div className="mr-2"/>
-
-                        <DropdownButton className="ml-auto" id="dropdown-item-button-resize" title={this.props.pageSize+ " " + this.props.itemName + " per page"} size="sm">
-                            <Dropdown.Item as="button" onClick={() => this.repaginate(10)}>10 {this.props.itemName} per page</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={() => this.repaginate(15)}>15 {this.props.itemName} per page</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={() => this.repaginate(25)}>25 {this.props.itemName} per page</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={() => this.repaginate(50)}>50 {this.props.itemName} per page</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={() => this.repaginate(100)}>100 {this.props.itemName} per page</Dropdown.Item>
-                        </DropdownButton>
+                        </div>
 
                     </div>
+
                 </div>
-            );
-        } else {
-            return ( <div></div> );
-        }
+            </div>
+        );
+
     }
 }
 
