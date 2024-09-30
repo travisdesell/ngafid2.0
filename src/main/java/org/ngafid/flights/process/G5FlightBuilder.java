65b8a74c0becbd216ae0ba213316816f93f0490a
Joshua Karns <jkarns275@gmail.com>
2024-09-30 15:40:40 -0400

    Adding G5 builder

---
+++ src/main/java/org/ngafid/flights/process/G5FlightBuilder.java
@@ -0,0 +1,32 @@
+package org.ngafid.flights.process;
+
+import java.sql.Connection;
+
+import java.util.Map;
+import java.util.List;
+
+import org.ngafid.flights.*;
+
+/**
+ * Flight builder for G5FlightBuilder
+ */
+public class G5FlightBuilder extends FlightBuilder {
+
+  public G5FlightBuilder(FlightMeta meta, Map<String, DoubleTimeSeries> doubleTimeSeries,
+      Map<String, StringTimeSeries> stringTimeSeries) {
+    super(meta, doubleTimeSeries, stringTimeSeries);
+  }
+
+  private static final List<ProcessStep.Factory> processSteps = List.of(
+      ProcessAltAGL::new);
+
+  // This can be overridden.
+  protected List<ProcessStep> gatherSteps(Connection connection) {
+    // Add all of our processing steps here...
+    // The order doesn't matter; the DependencyGraph will resolve
+    // the order in the event that there are dependencies.
+    List<ProcessStep> steps = super.gatherSteps(connection);
+    processSteps.stream().map(factory -> factory.create(connection, this)).forEach(steps::add);
+    return steps;
+  }
+}
