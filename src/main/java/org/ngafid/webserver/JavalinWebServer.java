48ab2ba2e00e6574c93c453cc34f744c8fbe8234
Aaron Chan <aarchan108@gmail.com>
2024-10-08 15:12:45 -0400

    Probably implemented the auth checks function correctly

--- src/main/java/org/ngafid/webserver/JavalinWebServer.java
+++ src/main/java/org/ngafid/webserver/JavalinWebServer.java
@@ -1,11 +1,11 @@
 package org.ngafid.webserver;
 
 import io.javalin.Javalin;
-import io.javalin.config.JavalinConfig;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.ngafid.WebServer;
+import org.ngafid.accounts.User;
+import spark.Spark;
 
-import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class JavalinWebServer extends WebServer {
@@ -14,6 +14,7 @@ public class JavalinWebServer extends WebServer {
 
     public JavalinWebServer(int port, String staticFilesLocation) {
         super(port, staticFilesLocation);
+        app.start();
     }
 
     @Override
@@ -24,7 +25,6 @@ public class JavalinWebServer extends WebServer {
             LOG.info(ctx.method() + " " + ctx.path() + " took " + ms + "ms");
         });
     }
-
 
     @Override
     protected void configurePort() {
@@ -54,7 +54,52 @@ public class JavalinWebServer extends WebServer {
 
     @Override
     protected void configureAuthChecks() {
+        app.before("/protected/*", ctx -> {
+            LOG.info("protected URI: " + ctx.path());
 
+            User user = ctx.sessionAttribute("user");
+            String previousURI = ctx.sessionAttribute("previous_uri");
+
+            if (user == null) {
+                LOG.info("request uri: '" + ctx.path() + "'");
+                LOG.info("request url: '" + ctx.url() + "'");
+                LOG.info("request queryString: '" + ctx.queryString() + "'");
+
+                if (ctx.queryString() != null) {
+                    ctx.sessionAttribute("previous_uri", ctx.url() + "?" + ctx.queryString());
+                } else {
+                    ctx.sessionAttribute("previous_uri", ctx.url());
+                }
+
+                LOG.info("redirecting to access_denied");
+                ctx.redirect("/access_denied");
+                ctx.status(401).result("Go Away!");
+
+            } else if (!ctx.path().equals("/protected/waiting") && !user.hasViewAccess(user.getFleetId())) {
+                LOG.info("user waiting status, redirecting to waiting page!");
+                ctx.redirect("/protected/waiting");
+                ctx.status(401).result("Go Away!");
+
+            } else if (previousURI != null) {
+                ctx.redirect(previousURI);
+                ctx.sessionAttribute("previous_uri", null);
+            }
+        });
+
+        app.before("/", ctx -> {
+            User user = ctx.sessionAttribute("user");
+            if (user != null) {
+                String previousURI = ctx.sessionAttribute("previous_uri");
+                if (previousURI != null) {
+                    LOG.info("user already logged in, redirecting to the previous page because previous URI was not null");
+                    ctx.redirect(previousURI);
+                    ctx.sessionAttribute("previous_uri", null);
+                } else {
+                    LOG.info("user already logged in but accessing the '/' route, redirecting to welcome!");
+                    ctx.redirect("/protected/welcome");
+                }
+            }
+        });
     }
 
     @Override
