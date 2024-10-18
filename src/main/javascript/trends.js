fcaa419d97373578e214a8a1acf1802c22ddd97d
Ilverism <72947008+Ilverism@users.noreply.github.com>
2024-10-18 13:43:50 -0400

    Orange/Blue Graph Color Fix (#164)
    Fix color mismatches

--- src/main/javascript/trends.js
+++ src/main/javascript/trends.js
@@ -363,9 +363,15 @@ class TrendsPage extends React.Component {
 
 
             for (let [airframe, value] of Object.entries(countsObject)) {
-                if (value.airframeName === "Garmin Flight Display") continue;
-                if (selectedAirframe !== value.airframeName && selectedAirframe !== "All Airframes") continue;
 
+                //Airframe name is 'Garmin Flight Display', skip
+                if (value.airframeName === "Garmin Flight Display")
+                    continue;
+
+                //Current airframe name is neither the selected airframe name or 'All Airframes', skip
+                if ((selectedAirframe !== value.airframeName) && (selectedAirframe !== "All Airframes"))
+                    continue;
+        
                 /*
                 console.log("airframes, airframeName, value:");
                 console.log(airframes);
@@ -446,7 +452,8 @@ class TrendsPage extends React.Component {
         */
 
         for (let [eventName, fleetValue] of Object.entries(eventFleetPercents)) {
-            let ngafidValue = eventNGAFIDPercents[eventName];
+
+            //Push fleet values...
             if (!this.state.aggregatePage) {
                 percentData.push(fleetValue);
                 fleetValue.x = [];
@@ -472,6 +479,9 @@ class TrendsPage extends React.Component {
                     fleetValue.hovertext.push(fixedText  + " (" + fleetValue.flightsWithEventCounts[date] + " of " + fleetValue.totalFlightsCounts[date] + " flights) : " + fleetValue.name);
                 }
             }
+
+            //Push NGAFID data...
+            let ngafidValue = eventNGAFIDPercents[eventName];
             percentData.push(ngafidValue);
             ngafidValue.x = [];
             ngafidValue.y = [];
@@ -496,7 +506,6 @@ class TrendsPage extends React.Component {
                 ngafidValue.hovertext.push(fixedText + " (" + ngafidValue.flightsWithEventCounts[date] + " of " + ngafidValue.totalFlightsCounts[date] + " flights) : " + ngafidValue.name);
             }
 
-            //console.log(ngafidValue);
         }
 
         /*
