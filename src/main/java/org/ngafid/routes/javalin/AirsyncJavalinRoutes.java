4f2beead5f623864802af659ff5b78ca4dd00ac8
Aaron Chan <aarchan108@gmail.com>
2024-11-12 14:25:22 -0500

    AirsyncJavalinRoutes binded

--- src/main/java/org/ngafid/routes/javalin/AirsyncJavalinRoutes.java
+++ src/main/java/org/ngafid/routes/javalin/AirsyncJavalinRoutes.java
@@ -1,5 +1,6 @@
 package org.ngafid.routes.javalin;
 
+import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.ngafid.Database;
 import org.ngafid.WebServer;
@@ -29,7 +30,7 @@ import static org.ngafid.flights.AirSyncImport.getNumImports;
 public class AirsyncJavalinRoutes {
     private static final Logger LOG = Logger.getLogger(AirsyncJavalinRoutes.class.getName());
 
-    public static void getAirsyncImports(Context ctx) throws IOException {
+    private static void getAirsyncImports(Context ctx) throws IOException {
         final String templateFile = "airsync_imports.html";
 
         try (Connection connection = Database.getConnection()) {
@@ -64,7 +65,7 @@ public class AirsyncJavalinRoutes {
         }
     }
 
-    public static void getAirsyncUploads(Context ctx) throws IOException {
+    private static void getAirsyncUploads(Context ctx) throws IOException {
         String templateFile = WebServer.MUSTACHE_TEMPLATE_DIR + "airsync_uploads.html";
         LOG.severe("template file: '" + templateFile + "'");
 
@@ -103,7 +104,7 @@ public class AirsyncJavalinRoutes {
         }
     }
 
-    public static void postAirsyncImports(Context ctx) {
+    private static void postAirsyncImports(Context ctx) {
         User user = ctx.sessionAttribute("user");
         if (user == null) {
             LOG.severe("INVALID ACCESS: user was not logged in.");
@@ -136,7 +137,7 @@ public class AirsyncJavalinRoutes {
         }
     }
 
-    public static void postAirsyncUploads(Context ctx) {
+    private static void postAirsyncUploads(Context ctx) {
         User user = ctx.sessionAttribute("user");
         if (user == null) {
             LOG.severe("INVALID ACCESS: user was not logged in.");
@@ -168,7 +169,7 @@ public class AirsyncJavalinRoutes {
         }
     }
 
-    public static void postAirsyncManualUpdate(Context ctx) {
+    private static void postAirsyncManualUpdate(Context ctx) {
         User user = ctx.sessionAttribute("user");
         if (user == null) {
             LOG.severe("INVALID ACCESS: user was not logged in.");
@@ -207,7 +208,7 @@ public class AirsyncJavalinRoutes {
         }
     }
 
-    public static void postAirsyncTimeout(Context ctx) {
+    private static void postAirsyncTimeout(Context ctx) {
         User user = ctx.sessionAttribute("user");
         if (user == null) {
             LOG.severe("INVALID ACCESS: user was not logged in.");
@@ -239,5 +240,15 @@ public class AirsyncJavalinRoutes {
             e.printStackTrace();
             ctx.json(new ErrorResponse(e));
         }
+    }
+    
+    public static void bindRoutes(Javalin app) {
+        app.get("/protected/airsync_uploads", AirsyncJavalinRoutes::getAirsyncUploads);
+        app.post("/protected/airsync_uploads", AirsyncJavalinRoutes::postAirsyncUploads);
+
+        app.get("/protected/airsync_imports", AirsyncJavalinRoutes::getAirsyncImports);
+        app.post("/protected/airsync_update", AirsyncJavalinRoutes::postAirsyncManualUpdate);
+        app.post("/protected/airsync_imports", AirsyncJavalinRoutes::postAirsyncImports);
+        app.post("/protected/airsync_settings", AirsyncJavalinRoutes::postAirsyncTimeout);
     }
 }
