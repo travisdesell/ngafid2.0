71ca19611028ae48b5e0e26e3967e1cc7bf0ef02
Aaron Chan <aarchan108@gmail.com>
2024-12-23 18:06:21 -0500

    Fix redundant modifiers in CalculationParameter

--- src/main/java/org/ngafid/flights/calculations/CalculationParameters.java
+++ src/main/java/org/ngafid/flights/calculations/CalculationParameters.java
@@ -6,11 +6,12 @@
 
 package org.ngafid.flights.calculations;
 
-import java.util.Map;
+import org.ngafid.flights.Airframes;
+import org.ngafid.flights.DoubleTimeSeries;
 
 public interface CalculationParameters {
-    public static final double STD_PRESS_INHG = 29.92;
-    public static final double COMP_CONV = (double) (Math.PI / 180); 
+    double STD_PRESS_INHG = 29.92;
+    double COMP_CONV = Math.PI / 180;
 
     /**
      * Critical Values
@@ -18,75 +19,75 @@ public interface CalculationParameters {
      * @param AOA_CRIT this is the critical angle of attack that can be changes based on the FDM's guidelines
      * @param PROSPIN_LIM this is the crirtical value for the "Coordination Index", which can also be changed based on certain guidelines
      */
-    public static final double AOA_CRIT = 15;
-    public static final double PROSPIN_LIM = 4;
+    double AOA_CRIT = 15;
+    double PROSPIN_LIM = 4;
 
-    public static final int YAW_RATE_LAG = 1;
-    public static final int VSI_LAG_DIFF = 1;
+    int YAW_RATE_LAG = 1;
+    int VSI_LAG_DIFF = 1;
 
     /**
      * {@link DoubleTimeSeries} constants
      */
-    public static final String LAG_SUFFIX = "_lag";
-    public static final String LEAD_SUFFIX = "_lead";
-    public static final String HDG = "HDG";
-    public static final String NORM_AC = "NormAc";
-    public static final String LAT_AC = "LatAc";
-    public static final String IAS = "IAS";
-    public static final String VSPD = "VSPD";
-    public static final String DENSITY_RATIO = "DensityRatio";
-    public static final String OAT = "OAT";
-    public static final String BARO_A = "BaroA";
-    public static final String PITCH = "Pitch";
-    public static final String ROLL = "Roll";
-    public static final String ALT_AGL = "AltAGL";
-    public static final String ALT_MSL = "AltMSL";
-    public static final String ALT_B = "AltB";
-    public static final String AOA_SIMPLE = "AOASimple"; 
-    public static final String E1_RPM = "E1 RPM";
-    public static final String TAS_FTMIN = "True Airspeed(ft/min)";
-    public static final String STALL_PROB = "Stall Index";
-    public static final String SPIN = "Spin Event(s)";
-    public static final String LOCI = "LOC-I Index";
-    public static final String PRO_SPIN_FORCE = "Coordination Index";
-    public static final String YAW_RATE = "Yaw Rate";
-    public static final String VSPD_CALCULATED = "VSpd Calculated";
-    public static final String CAS = "CAS";
-    public static final String GND_SPD = "GndSpd";
-    public static final String WIND_SPEED = "WndSpd";
-    public static final String WIND_DIRECTION = "WndDr";
-    public static final String TOTAL_FUEL = "Total Fuel";
-    public static final String LCL_DATE = "Lcl Date";
-    public static final String LCL_TIME = "Lcl Time";
+    String LAG_SUFFIX = "_lag";
+    String LEAD_SUFFIX = "_lead";
+    String HDG = "HDG";
+    String NORM_AC = "NormAc";
+    String LAT_AC = "LatAc";
+    String IAS = "IAS";
+    String VSPD = "VSPD";
+    String DENSITY_RATIO = "DensityRatio";
+    String OAT = "OAT";
+    String BARO_A = "BaroA";
+    String PITCH = "Pitch";
+    String ROLL = "Roll";
+    String ALT_AGL = "AltAGL";
+    String ALT_MSL = "AltMSL";
+    String ALT_B = "AltB";
+    String AOA_SIMPLE = "AOASimple";
+    String E1_RPM = "E1 RPM";
+    String TAS_FTMIN = "True Airspeed(ft/min)";
+    String STALL_PROB = "Stall Index";
+    String SPIN = "Spin Event(s)";
+    String LOCI = "LOC-I Index";
+    String PRO_SPIN_FORCE = "Coordination Index";
+    String YAW_RATE = "Yaw Rate";
+    String VSPD_CALCULATED = "VSpd Calculated";
+    String CAS = "CAS";
+    String GND_SPD = "GndSpd";
+    String WIND_SPEED = "WndSpd";
+    String WIND_DIRECTION = "WndDr";
+    String TOTAL_FUEL = "Total Fuel";
+    String LCL_DATE = "Lcl Date";
+    String LCL_TIME = "Lcl Time";
 
     /**
      * {@link Airframes} id's
      */
-    public static final int C172SP_ID = 1;
+    int C172SP_ID = 1;
 
     /**
      * Strings that represent the parameters used in the Stall Index calculation
      *
-     * @param ALT_B is used as the time reference 
+     * @param ALT_B is used as the time reference
      * @param VSPD not needed for cases where VSpd is drived from AltB
      */
-    public static final String [] STALL_DEPENDENCIES = {PITCH, /*VSPD,*/ IAS, BARO_A, OAT, ALT_B};
-    
+    String[] STALL_DEPENDENCIES = {PITCH, /*VSPD,*/ IAS, BARO_A, OAT, ALT_B};
+
     /**
      * Strings that represent the parameters used in the Stall Index calculation
      */
-    public static final String [] LOCI_DEPENDENCIES = {HDG, ROLL};
+    String[] LOCI_DEPENDENCIES = {HDG, ROLL};
     //
     // use these for a real true airspeed (Shelbys method) /*GND_SPD, WIND_SPEED, WIND_DIRECTION};*/
-    public static final String [] SPIN_DEPENDENCIES = {IAS, VSPD_CALCULATED, NORM_AC, LAT_AC, ALT_AGL};
+    String[] SPIN_DEPENDENCIES = {IAS, VSPD_CALCULATED, NORM_AC, LAT_AC, ALT_AGL};
 
     // Used to determine average fuel
-    public static final String[] AVG_FUEL_DEPENDENCIES = {TOTAL_FUEL};
+    String[] AVG_FUEL_DEPENDENCIES = {TOTAL_FUEL};
 
     /**
-     * Strings that represent the supplementary metrics displayed in the UI
+     * Strings that represent the supplementary metrics displayed in the UI.
      */
-    public static final String [] uiMetrics = {ROLL, IAS, PITCH, ALT_MSL, AOA_SIMPLE, E1_RPM, ALT_AGL};
-    public static final String [] defaultMetrics = {ROLL, PITCH, IAS, ALT_MSL, ALT_AGL, AOA_SIMPLE, E1_RPM};
+    String[] UI_METRICS = {ROLL, IAS, PITCH, ALT_MSL, AOA_SIMPLE, E1_RPM, ALT_AGL};
+    String[] DEFAULT_METRICS = {ROLL, PITCH, IAS, ALT_MSL, ALT_AGL, AOA_SIMPLE, E1_RPM};
 
 }
