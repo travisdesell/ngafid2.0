088ec37ce7f125c018fcd0f818fba1570ece2640
Aidan Cusack <aidanqsack@gmail.com>
2025-01-15 16:40:42 -0500

    Manual event stats fetch start script

---
+++ event_stats/event_stats_manual_start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# - Call with 'bash event_stats/event_stats_manual_start.sh'
+# - Manually triggers the event stats call
+# - ⚠ Make sure that this file is run from the NGAFID root directory, otherwise the command will not work
+
+
+#Source the configuration script 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$SCRIPT_DIR/event_stats_variables_config.sh"
+
+
+#Enable the cron job
+printf "\n--------------------\nManual Fetch Started\n--------------------\n\n" >> $SCRIPT_DIR/EventStatisticsFetch.log
+
+
+#Manually trigger the event stats call
+MANUAL_CMD="cd $SCRIPT_DIR && cd ../ && . ./init_env.sh && $JAVA_HOME/bin/java -cp ./target/ngafid-1.0-SNAPSHOT.jar org.ngafid.events.EventStatisticsFetch"
+echo "Manually starting the Event Statistics Fetch Java program... (Using Java Home: $JAVA_HOME)"
+
+eval "$MANUAL_CMD"
