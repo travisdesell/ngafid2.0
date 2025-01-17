69c6f5d5310b1941e9429cc4ca934c213c32fc5f
Aaron Chan <aarchan108@gmail.com>
2025-01-17 12:05:04 -0500

    Blast the gson code from Webserver

--- src/main/java/org/ngafid/WebServer.java
+++ src/main/java/org/ngafid/WebServer.java
@@ -1,21 +1,13 @@
 package org.ngafid;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
 import org.ngafid.accounts.EmailType;
 import org.ngafid.common.ConvertToHTML;
 import org.ngafid.webserver.JavalinWebServer;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -29,40 +21,10 @@ import static org.ngafid.SendEmail.sendAdminEmails;
  * @author <a href='mailto:tjdvse@rit.edu'>Travis Desell</a>
  */
 public abstract class WebServer {
-    private static final Logger LOG = Logger.getLogger(WebServer.class.getName());
-
     public static final String NGAFID_UPLOAD_DIR;
     public static final String NGAFID_ARCHIVE_DIR;
     public static final String MUSTACHE_TEMPLATE_DIR;
-
-    protected final String staticFilesLocation;
-    protected final int port;
-
-    protected final int maxThreads = 32;
-    protected final int minThreads = 2;
-    protected final int timeOutMillis = 1000 * 60 * 5;
-
-    public static class LocalDateTimeTypeAdapter extends TypeAdapter<LocalDateTime> {
-        @Override
-        public void write(final JsonWriter jsonWriter, final LocalDateTime localDate) throws IOException {
-            if (localDate == null) {
-                jsonWriter.nullValue();
-                return;
-            }
-            jsonWriter.value(localDate.toString());
-        }
-
-        @Override
-        public LocalDateTime read(final JsonReader jsonReader) throws IOException {
-            if (jsonReader.peek() == JsonToken.NULL) {
-                jsonReader.nextNull();
-                return null;
-            }
-            return ZonedDateTime.parse(jsonReader.nextString()).toLocalDateTime();
-        }
-    }
-
-    public static final Gson gson = new GsonBuilder().serializeSpecialFloatingPointValues().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter()).create();
+    private static final Logger LOG = Logger.getLogger(WebServer.class.getName());
 
     static {
         NGAFID_UPLOAD_DIR = getEnvironmentVariable("NGAFID_UPLOAD_DIR");
@@ -70,11 +32,21 @@ public abstract class WebServer {
         MUSTACHE_TEMPLATE_DIR = getEnvironmentVariable("MUSTACHE_TEMPLATE_DIR");
 
         // Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        //     String message = "NGAFID WebServer has shutdown at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
+        //     String message = "NGAFID WebServer has shutdown at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy
+        //     HH:mm:ss"));
         //     LOG.info(message);
         //     sendAdminEmails(message, "", EmailType.ADMIN_SHUTDOWN_NOTIFICATION);
         // }));
     }
+
+    protected final String staticFilesLocation;
+    protected final int port;
+    protected final int maxThreads = 32;
+    protected final int minThreads = 2;
+    protected final int timeOutMillis = 1000 * 60 * 5;
+
+// Register a custom serializer/deserializer for LocalDateTime
+
 
     public WebServer(int port, String staticFilesLocation) {
         this.port = port;
@@ -98,7 +70,36 @@ public abstract class WebServer {
         configureExceptions();
     }
 
-    protected void preInitialize() {};
+    public static String getEnvironmentVariable(String key) {
+        String value = System.getenv(key);
+        if (value == null) {
+            System.err.println("ERROR: '" + key + "' environment variable not specified at runtime.");
+            System.err.println("Please add the following to your ~/.bash_rc or ~/.profile file:");
+            System.err.println("export " + key + "=<value>");
+            throw new RuntimeException("Environment variable '" + key + "' not set.");
+        }
+
+        return value;
+    }
+
+    /**
+     * Entry point for the NGAFID web server.
+     *
+     * @param args Command line arguments; none expected.
+     */
+    public static void main(String[] args) {
+        String staticFiles = getEnvironmentVariable("WEBSERVER_STATIC_FILES");
+        int port = Integer.parseInt(getEnvironmentVariable("NGAFID_PORT"));
+
+        // The application uses Jackson to generate JSON representations of Java objects.
+        // This should be used by your Ajax Routes to generate JSON for the HTTP
+        // response to Ajax requests.
+        WebServer webserver = new JavalinWebServer(port, staticFiles);
+        LOG.info("NGAFID web server initialization complete.");
+    }
+
+    protected void preInitialize() {
+    }
 
     protected abstract void configurePort();
 
@@ -124,9 +125,16 @@ public abstract class WebServer {
         String stackTrace = sw.toString(); // stack trace as a string
         LOG.severe("stack trace:\n" + stackTrace);
 
-        String message = "An uncaught exception was thrown in the NGAFID SparkWebServer at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss")) + ".\n The exception was: " + exception + "\n" + ".\n The exception message was: " + exception.getMessage() + "\n" + ".\n The exception (to string): " + exception + "\n" + "\n The non-pretty stack trace is:\n" + stackTrace + "\n" + "\nThe stack trace was:\n" + ConvertToHTML.convertError(exception) + "\n";
+        String message =
+                "An uncaught exception was thrown in the NGAFID SparkWebServer at " +
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss")) +
+                        ".\n The exception was: " + exception + "\n" + ".\n The exception message was: " +
+                        exception.getMessage() + "\n" + ".\n The exception (to string): " + exception + "\n" +
+                        "\n The non-pretty stack trace is:\n" + stackTrace + "\n" + "\nThe stack trace was:\n" +
+                        ConvertToHTML.convertError(exception) + "\n";
 
-        sendAdminEmails(String.format("Uncaught Exception in NGAFID: %s", exception.getMessage()), ConvertToHTML.convertString(message), EmailType.ADMIN_EXCEPTION_NOTIFICATION);
+        sendAdminEmails(String.format("Uncaught Exception in NGAFID: %s", exception.getMessage()), ConvertToHTML.convertString(message),
+                EmailType.ADMIN_EXCEPTION_NOTIFICATION);
     }
 
     protected void configureLogging() {
@@ -139,33 +147,5 @@ public abstract class WebServer {
             e.printStackTrace();
             System.err.println("Could not initialize log manager because: " + e.getMessage());
         }
-    }
-
-    public static String getEnvironmentVariable(String key) {
-        String value = System.getenv(key);
-        if (value == null) {
-            System.err.println("ERROR: '" + key + "' environment variable not specified at runtime.");
-            System.err.println("Please add the following to your ~/.bash_rc or ~/.profile file:");
-            System.err.println("export " + key + "=<value>");
-            throw new RuntimeException("Environment variable '" + key + "' not set.");
-        }
-
-        return value;
-    }
-
-    /**
-     * Entry point for the NGAFID web server.
-     *
-     * @param args Command line arguments; none expected.
-     */
-    public static void main(String[] args) {
-        String staticFiles = getEnvironmentVariable("WEBSERVER_STATIC_FILES");
-        int port = Integer.parseInt(getEnvironmentVariable("NGAFID_PORT"));
-
-        // The application uses Gson to generate JSON representations of Java objects.
-        // This should be used by your Ajax Routes to generate JSON for the HTTP
-        // response to Ajax requests.
-        WebServer webserver = new JavalinWebServer(port, staticFiles);
-        LOG.info("NGAFID web server initialization complete.");
     }
 }
