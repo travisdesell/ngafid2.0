4c99430191faf23bc4f1fea15d26859678581003
Aidan Cusack <aidanqsack@gmail.com>
2025-02-03 16:48:35 -0500

    ProcessLOCI NaN checks

--- src/main/java/org/ngafid/uploads/process/steps/ProcessLOCI.java
+++ src/main/java/org/ngafid/uploads/process/steps/ProcessLOCI.java
@@ -1,10 +1,5 @@
 package org.ngafid.uploads.process.steps;
 
-import org.ngafid.flights.DoubleTimeSeries;
-import org.ngafid.uploads.process.FatalFlightFileException;
-import org.ngafid.uploads.process.format.FlightBuilder;
-import org.ngafid.uploads.process.MalformedFlightFileException;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collections;
@@ -12,59 +7,87 @@ import java.util.Set;
 import java.util.logging.Logger;
 
 import static org.ngafid.flights.Airframes.AIRFRAME_CESSNA_172S;
-import static org.ngafid.flights.Flight.calculateLOCI;
-import static org.ngafid.flights.Parameters.*;
+import org.ngafid.flights.DoubleTimeSeries;
+import org.ngafid.flights.Flight;
+import static org.ngafid.flights.Parameters.HDG;
+import static org.ngafid.flights.Parameters.LOCI;
+import static org.ngafid.flights.Parameters.LOCI_DEPENDENCIES;
+import static org.ngafid.flights.Parameters.PRO_SPIN_FORCE;
+import static org.ngafid.flights.Parameters.ROLL;
+import static org.ngafid.flights.Parameters.STALL_PROB;
+import static org.ngafid.flights.Parameters.TAS_FTMIN;
+import org.ngafid.flights.Parameters.Unit;
+import static org.ngafid.flights.Parameters.YAW_RATE_LAG;
+import org.ngafid.uploads.process.FatalFlightFileException;
+import org.ngafid.uploads.process.MalformedFlightFileException;
+import org.ngafid.uploads.process.format.FlightBuilder;
 
 public class ProcessLOCI extends ProcessStep {
-    private static final Logger LOG = Logger.getLogger(ProcessLOCI.class.getName());
+	private static final Logger LOG = Logger.getLogger(ProcessLOCI.class.getName());
 
-    private static final Set<String> REQUIRED_DOUBLE_COLUMNS = Set.of(LOCI_DEPENDENCIES);
+	private static final Set<String> REQUIRED_DOUBLE_COLUMNS = Set.of(LOCI_DEPENDENCIES);
 
-    public ProcessLOCI(Connection connection, FlightBuilder builder) {
-        super(connection, builder);
-    }
+	public ProcessLOCI(Connection connection, FlightBuilder builder) {
+    	super(connection, builder);
+	}
 
-    public Set<String> getRequiredDoubleColumns() {
-        return REQUIRED_DOUBLE_COLUMNS;
-    }
+	public Set<String> getRequiredDoubleColumns() {
+    	return REQUIRED_DOUBLE_COLUMNS;
+	}
 
-    public Set<String> getRequiredStringColumns() {
-        return Collections.emptySet();
-    }
+	public Set<String> getRequiredStringColumns() {
+    	return Collections.emptySet();
+	}
 
-    public Set<String> getRequiredColumns() {
-        return REQUIRED_DOUBLE_COLUMNS;
-    }
+	public Set<String> getRequiredColumns() {
+    	return REQUIRED_DOUBLE_COLUMNS;
+	}
 
-    public Set<String> getOutputColumns() {
-        return Collections.emptySet();
-    }
+	public Set<String> getOutputColumns() {
+    	return Collections.emptySet();
+	}
 
-    public boolean airframeIsValid(String airframe) {
-        return airframe.equals(AIRFRAME_CESSNA_172S);
-    }
+	public boolean airframeIsValid(String airframe) {
+    	return airframe.equals(AIRFRAME_CESSNA_172S);
+	}
 
-    public void compute() throws SQLException, MalformedFlightFileException, FatalFlightFileException {
-        DoubleTimeSeries hdg = builder.getDoubleTimeSeries(HDG);
-        DoubleTimeSeries hdgLagged = hdg.lag(YAW_RATE_LAG);
-        DoubleTimeSeries roll = builder.getDoubleTimeSeries(ROLL);
-        DoubleTimeSeries tas = builder.getDoubleTimeSeries(TAS_FTMIN);
-        DoubleTimeSeries stallIndex = builder.getDoubleTimeSeries(STALL_PROB);
+	public void compute() throws SQLException, MalformedFlightFileException, FatalFlightFileException {
 
-        int length = roll.size();
+    	DoubleTimeSeries hdg = builder.getDoubleTimeSeries(HDG);
+    	DoubleTimeSeries hdgLagged = hdg.lag(YAW_RATE_LAG);
+    	DoubleTimeSeries roll = builder.getDoubleTimeSeries(ROLL);
+    	DoubleTimeSeries tas = builder.getDoubleTimeSeries(TAS_FTMIN);
+    	DoubleTimeSeries stallIndex = builder.getDoubleTimeSeries(STALL_PROB);
 
-        DoubleTimeSeries coordIndex = DoubleTimeSeries.computed(PRO_SPIN_FORCE, "index", length,
-                (int index) -> {
-                    double laggedHdg = hdgLagged.get(index);
-                    return calculateLOCI(hdg, index, roll, tas, laggedHdg);
-                });
-        DoubleTimeSeries loci = DoubleTimeSeries.computed(LOCI, "index", length,
-                index -> {
-                    double prob = stallIndex.get(index) * coordIndex.get(index);
-                    return prob / 100;
-                });
+    	int length = roll.size();
 
-        builder.addTimeSeries(coordIndex);
-        builder.addTimeSeries(loci);
-    }
+    	DoubleTimeSeries coordIndex;
+    	coordIndex = DoubleTimeSeries.computed(PRO_SPIN_FORCE, Unit.INDEX, length, index -> {
+
+        	double rollValue = roll.get(index);
+        	double tasValue = tas.get(index);
+        	double laggedHdgValue = hdgLagged.get(index);
+
+        	if (Double.isNaN(rollValue) || Double.isNaN(tasValue) || Double.isNaN(laggedHdgValue))
+            	return 0.0;
+
+        	double valueOut = Flight.calculateLOCI(hdg, index, roll, tas, laggedHdgValue);
+        	return valueOut;
+
+    	});
+
+    	DoubleTimeSeries loci = DoubleTimeSeries.computed(LOCI, "index", length, index -> {
+
+            double prob = stallIndex.get(index) * coordIndex.get(index);
+            if (Double.isNaN(prob))
+                return 0.0;
+
+            return (prob / 100);
+            
+        });
+
+    	builder.addTimeSeries(coordIndex);
+    	builder.addTimeSeries(loci);
+
+	}
 }
