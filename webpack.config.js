4b7a66a39c06f6f7734c71b85b728b57511ace06
Aaron Chan <aarchan108@gmail.com>
2025-02-03 14:03:25 -0500

    Ignore vite for now. Use webpack 5. Dont need to force install and npm run dev works now but homepage is borked

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-var path = require('path');
-
+const path = require('path');
 
 module.exports = {
+    mode: process.env.NODE_ENV || 'development',
     watch: true,
-
 
     resolve: {
         fallback: {
@@ -15,20 +13,11 @@ module.exports = {
         },
     },
 
-
-    /*
-    node: {
-        fs: 'empty'
-    },
-    */
-
-
     watchOptions: {
         aggregateTimeout: 300,
         poll: 1000,
         ignored: /node_modules/
     },
-
 
     optimization: {
         removeAvailableModules: false,
@@ -36,105 +25,96 @@ module.exports = {
         splitChunks: false,
     },
 
-
-    entry: {
-        aggregate: __dirname + "/src/main/javascript/aggregate.js",
-        aggregate_trends : __dirname + "/src/main/javascript/aggregate_trends.js",
-        airsync_imports: __dirname + "/src/main/javascript/airsync_imports.js",
-        airsync_uploads: __dirname + "/src/main/javascript/airsync_uploads.js",
-        create_account: __dirname + "/src/main/javascript/create_account.js",
-        create_event: __dirname + "/src/main/javascript/create_event.js",
-        event_definition: __dirname + "/src/main/javascript/event_definition.js",
-        event_definitions_display: __dirname + "/src/main/javascript/event_definitions_display.js",
-        event_statistics: __dirname + "/src/main/javascript/event_statistics.js",
-        fleet_trends: __dirname+ "/src/main/javascript/fleet_trends.js",
-        flight: __dirname + "/src/main/javascript/flight.js",
-        flight_display: __dirname + "/src/main/javascript/flight_display.js",
-        flights: __dirname + "/src/main/javascript/flights.js",
-        forgot_password: __dirname + "/src/main/javascript/forgot_password.js",
-        signed_in_navbar: __dirname + "/src/main/javascript/signed_in_navbar.js",
-        home_navbar: __dirname + "/src/main/javascript/home_navbar.js",
-        imports: __dirname + "/src/main/javascript/imports.js",
-        manage_events: __dirname + "/src/main/javascript/manage_events.js",
-        manage_fleet: __dirname + "/src/main/javascript/manage_fleet.js",
-        // ngafid_cesium: __dirname + "/src/main/javascript/ngafid_cesium.js",
-        reset_password: __dirname + "/src/main/javascript/reset_password.js",
-        severities: __dirname + "/src/main/javascript/severities.js",
-        system_ids: __dirname + "/src/main/javascript/system_ids.js",
-        // trends: __dirname + "/src/main/javascript/trends.js",
-        time_zones: __dirname + "/src/main/javascript/time_zones.js",
-        ttf: __dirname + "/src/main/javascript/ttf.js",
-        user_preferences: __dirname + "/src/main/javascript/preferences_page.js",
-        update_event: __dirname + "/src/main/javascript/update_event.js",
-        update_password: __dirname + "/src/main/javascript/update_password.js",
-        update_profile: __dirname + "/src/main/javascript/update_profile.js",
-        uploads: __dirname + "/src/main/javascript/uploads.js",
-        welcome: __dirname + "/src/main/javascript/welcome.js",
-        theme_preload: __dirname + "/src/main/javascript/theme_preload.js",
+    cache: {
+        type: 'filesystem',
+        cacheDirectory: path.resolve(__dirname, '.webpack_cache'),
     },
 
+    entry: {
+        aggregate: "./src/main/javascript/aggregate.js",
+        aggregate_trends: "./src/main/javascript/aggregate_trends.js",
+        airsync_imports: "./src/main/javascript/airsync_imports.js",
+        airsync_uploads: "./src/main/javascript/airsync_uploads.js",
+        create_account: "./src/main/javascript/create_account.js",
+        create_event: "./src/main/javascript/create_event.js",
+        event_definition: "./src/main/javascript/event_definition.js",
+        event_definitions_display: "./src/main/javascript/event_definitions_display.js",
+        event_statistics: "./src/main/javascript/event_statistics.js",
+        fleet_trends: "./src/main/javascript/fleet_trends.js",
+        flight: "./src/main/javascript/flight.js",
+        flight_display: "./src/main/javascript/flight_display.js",
+        flights: "./src/main/javascript/flights.js",
+        forgot_password: "./src/main/javascript/forgot_password.js",
+        signed_in_navbar: "./src/main/javascript/signed_in_navbar.js",
+        home_navbar: "./src/main/javascript/home_navbar.js",
+        imports: "./src/main/javascript/imports.js",
+        manage_events: "./src/main/javascript/manage_events.js",
+        manage_fleet: "./src/main/javascript/manage_fleet.js",
+        reset_password: "./src/main/javascript/reset_password.js",
+        severities: "./src/main/javascript/severities.js",
+        system_ids: "./src/main/javascript/system_ids.js",
+        time_zones: "./src/main/javascript/time_zones.js",
+        ttf: "./src/main/javascript/ttf.js",
+        user_preferences: "./src/main/javascript/preferences_page.js",
+        update_event: "./src/main/javascript/update_event.js",
+        update_password: "./src/main/javascript/update_password.js",
+        update_profile: "./src/main/javascript/update_profile.js",
+        uploads: "./src/main/javascript/uploads.js",
+        welcome: "./src/main/javascript/welcome.js",
+        theme_preload: "./src/main/javascript/theme_preload.js",
+    },
 
     devtool: "source-map",
 
-
     output: {
-        // Farhad: for webpackCesium
-        sourcePrefix: "",
-        //
-        path: __dirname + "/src/main/resources/public/js/",
+        path: path.resolve(__dirname, "src/main/resources/public/js/"),
         filename: "[name]-bundle.js"
     },
-
 
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [
-                    'cache-loader',
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            cacheDirectory: true,
-                        }
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
                     }
-                ],
+                },
                 include: path.resolve('src')
             },
-
-
             {
                 test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
+                use: ["html-loader"]
             },
             {
                 test: /\.mjs$/,
                 include: /node_modules/,
                 type: "javascript/auto",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
-
 
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
             'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default']
+            Popper: ['@popperjs/core', 'default']
         })
+    ],
 
-
-        /*
-        new HtmlWebPackPlugin({
-            template: "./src/main/resources/public/js3/src/index.html",
-            filename: "./index.html"
-        })
-        */
-    ]
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src/main/resources/public/'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    }
 };
