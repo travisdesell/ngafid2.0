194dd2238431c0b2a16622b744f19cf6e005292a
Roman Kozulia <romanmuni8@gmail.com>
2025-02-27 17:30:11 -0500

    cleanup

--- src/main/java/org/ngafid/bin/WebServer.java
+++ src/main/java/org/ngafid/bin/WebServer.java
@@ -10,10 +10,7 @@ import org.ngafid.accounts.EmailType;
 import org.ngafid.common.ConvertToHTML;
 import org.ngafid.routes.JavalinWebServer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
