9952b86663a431c0a1d7c65cbf302649cac0508a
e911 <pujanthapa88.63@gmail.com>
2025-05-12 14:14:57 -0400

    Update fix to alter column

--- ngafid-db/src/changelogs/00-accounts/03-fleet-access.sql
+++ ngafid-db/src/changelogs/00-accounts/03-fleet-access.sql
@@ -4,9 +4,13 @@
 CREATE TABLE fleet_access (
     user_id INT,
     fleet_id INT,
-    type ENUM('MANAGER', 'UPLOAD', 'VIEW', 'WAITING', 'DENIED'),
+    type VARCHAR(32),
 
     PRIMARY KEY (user_id, fleet_id),
     FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (fleet_id) REFERENCES fleet(id)
 );
+
+--changeset pujan:modify-fleet-access-type labels:accounts
+ALTER TABLE fleet_access
+    MODIFY COLUMN type ENUM('MANAGER', 'UPLOAD', 'VIEW', 'WAITING', 'DENIED');
