a67e8b37360e25bae0b5c53504b08bffff269dda
Aidan Cusack <aidanqsack@gmail.com>
2025-07-14 19:28:57 -0400

    Add Event interface

--- ngafid-frontend/src/types.d.ts
+++ ngafid-frontend/src/types.d.ts
@@ -22,6 +22,19 @@ export interface NGAFIDUser {
     isFleetManager: boolean;
 }
 
+
+export interface Event {
+
+    id: React.Key | null | undefined;
+    flightId: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined;
+    otherFlightId: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined;
+    startTime: string | number | Date;
+    endTime: string | number | Date;
+    severity: number;
+
+}
+
+
 //TODO: Figure out the best way to reconcile this with eslint globals
 declare global {
     const waitingUserCount: number;
