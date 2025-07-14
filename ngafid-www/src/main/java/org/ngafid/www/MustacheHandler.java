d2c31a6e867b916f00d1b5fecd048f9ba066ed9e
Aidan Cusack <aidanqsack@gmail.com>
2025-07-14 16:51:56 -0400

    Make Mustache Handler pass template name relative to root

--- ngafid-www/src/main/java/org/ngafid/www/MustacheHandler.java
+++ ngafid-www/src/main/java/org/ngafid/www/MustacheHandler.java
@@ -1,26 +1,28 @@
 package org.ngafid.www;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
-import io.javalin.http.Context;
-import io.javalin.rendering.FileRenderer;
-import io.javalin.util.JavalinLogger;
-import org.jetbrains.annotations.NotNull;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Map;
 
+import org.jetbrains.annotations.NotNull;
 import static org.ngafid.core.Config.MUSTACHE_TEMPLATE_DIR;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import io.javalin.http.Context;
+import io.javalin.rendering.FileRenderer;
+import io.javalin.util.JavalinLogger;
 
 public class MustacheHandler implements FileRenderer {
     public static String handle(String templateFilename, Map<String, ?> scopes) throws IOException {
-        MustacheFactory mf = new DefaultMustacheFactory();
+        MustacheFactory mf = new DefaultMustacheFactory(new File(MUSTACHE_TEMPLATE_DIR));
         String templateFile = MUSTACHE_TEMPLATE_DIR + "/" + templateFilename;
         JavalinLogger.info("handling mustache template: " + templateFile);
-        Mustache mustache = mf.compile(templateFile);
+        Mustache mustache = mf.compile(templateFilename);
         StringWriter stringOut = new StringWriter();
 
         mustache.execute(new PrintWriter(stringOut), scopes).flush();
