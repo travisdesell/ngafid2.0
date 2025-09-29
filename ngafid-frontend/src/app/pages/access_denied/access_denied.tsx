c58c8c1ac81cda34affed53162a071f0a090375c
Aidan Cusack <aidanqsack@gmail.com>
2025-09-29 19:28:14 -0400

    Move to Welcome page when trying to access a disallowed page while logged out

--- ngafid-frontend/src/app/pages/access_denied/access_denied.tsx
+++ ngafid-frontend/src/app/pages/access_denied/access_denied.tsx
@@ -1,4 +1,5 @@
 // ngafid-frontend/src/app/pages/access_denied/access_denied.tsx
+import { useAuth } from "@/auth";
 import ErrorModal, { ModalDataError } from "@/components/modals/error_modal";
 import { useModal } from "@/components/modals/modal_provider";
 import { useEffect } from "react";
@@ -6,13 +7,20 @@ import { useEffect } from "react";
 export default function AccessDenied() {
 
     const { setModal } = useModal();
+    const { user } = useAuth();
 
     useEffect(() => {
 
         setModal(
             ErrorModal,
-            {title : "Access Denied", message: "You do not have permission to view this page. Close this to return to the previous page."} as ModalDataError,
-            () => { window.history.back(); }
+            {title : "Access Denied", message: "You do not have permission to view this page. Close this to return to the previous page (or the home page if you're logged out)."} as ModalDataError,
+            () => {
+
+                if (!user)
+                    window.location.assign("/");
+                else
+                    window.history.back();
+            }
         );
 
     }, []);
