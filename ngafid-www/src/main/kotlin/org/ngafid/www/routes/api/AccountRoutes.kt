bb46bd493ea67a5c2b21d5eb296475955ba0962b
Aaron Chan <aarchan108@gmail.com>
2025-10-07 12:50:02 -0400

    ACcount routes stubbed class

---
+++ ngafid-www/src/main/kotlin/org/ngafid/www/routes/api/AccountRoutes.kt
@@ -0,0 +1,12 @@
+package org.ngafid.www.routes.api
+
+import io.javalin.config.JavalinConfig
+import org.ngafid.www.routes.RouteProvider
+
+class AccountRoutes : RouteProvider {
+    override fun bind(app: JavalinConfig) {
+        TODO("Not yet implemented")
+    }
+
+
+}
