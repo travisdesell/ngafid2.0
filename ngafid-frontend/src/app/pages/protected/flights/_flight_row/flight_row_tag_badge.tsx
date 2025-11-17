be2226aa2ad02078ff8dbe66928ec92938625a4d
Aidan Cusack <aidanqsack@gmail.com>
2025-11-17 12:47:24 -0500

    Flights: Fix TagData import in flight row tag badge component

--- ngafid-frontend/src/app/pages/protected/flights/_flight_row/flight_row_tag_badge.tsx
+++ ngafid-frontend/src/app/pages/protected/flights/_flight_row/flight_row_tag_badge.tsx
@@ -1,21 +1,25 @@
 // ngafid-frontend/src/app/pages/protected/flights/_flight_row/flight_row_tag_badge.tsx
 "use client"
 
+import type { TagData } from "@/components/providers/tags/tags_provider";
 import { Badge } from "@/components/ui/badge";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { Tag } from "lucide-react";
 
-type TestTag = {
-    name: string;
-    color: string;
-};
 
-export default function FlightRowTagBadge({ tag }: { tag: TestTag }) {
-
+export default function FlightRowTagBadge({ tag }: { tag: TagData }) {
     return (
-        <Badge className="rounded-full flex items-center gap-2 truncate" variant="secondary">
-            <Tag fill={tag.color} size={12}/>
-            <span className="truncate">{tag.name}</span>
-        </Badge>
+        <Tooltip>
+            <TooltipTrigger>
+                <Badge className="rounded-full flex items-center gap-2 truncate" variant="secondary">
+                    <Tag fill={tag.color} size={12}/>
+                    <span className="truncate">{tag.name}</span>
+                </Badge>
+            </TooltipTrigger>
+            <TooltipContent>
+                {tag.description}
+            </TooltipContent>
+        </Tooltip>
     );
 
 }
