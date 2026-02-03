6e019e6e72ef7ebae0eba1aa7d2d8ed71c3d76d8
Andrew Lee <leea7633@gmail.com>
2026-02-03 06:49:00 -0500

    test setup for upload flow test

--- ngafid-www/src/test/kotlin/org/ngafid/www/UploadFlowTest.kt
+++ ngafid-www/src/test/kotlin/org/ngafid/www/UploadFlowTest.kt
@@ -1,4 +1,32 @@
 package org.ngafid.www
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.edge.EdgeOptions
+
+
 class UploadFlowTest {
+    companion object {
+        private lateinit var driver: WebDriver
+        private const val baseUrl = "http://localhost:8181/"
+        private fun requireEnv(name: String): String =
+            System.getenv(name)
+                ?: throw IllegalStateException("Missing required env var: $name")
+        @JvmStatic
+        @BeforeAll
+        fun setUpDriver() {
+            val options = EdgeOptions()
+            options.addArguments("--headless=new", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage")
+            UploadFlowTest.Companion.driver = EdgeDriver(options)
+        }
+        @JvmStatic
+        @AfterAll
+        fun tearDown() {
+            driver.quit()
+        }
+        
+
+    }
 }
