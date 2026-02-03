481d14a13667e82b2160d87e4ffacc97bb5cff56
Andrew Lee <leea7633@gmail.com>
2026-02-03 08:55:08 -0500

    upload test fails due to not being able to find upload button

--- ngafid-www/src/test/kotlin/org/ngafid/www/UploadFlowTest.kt
+++ ngafid-www/src/test/kotlin/org/ngafid/www/UploadFlowTest.kt
@@ -5,7 +5,13 @@ import org.junit.jupiter.api.BeforeAll
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.edge.EdgeDriver
 import org.openqa.selenium.edge.EdgeOptions
-
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+import java.time.Duration
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.openqa.selenium.By
 
 class UploadFlowTest {
     companion object {
@@ -26,7 +32,51 @@ class UploadFlowTest {
         fun tearDown() {
             driver.quit()
         }
-        
-
     }
+    @Test
+    fun uploadSmallFlightFileSucceeds() {
+        val email = requireEnv("NGAFID_TEST_EMAIL")
+        val password = requireEnv("NGAFID_TEST_PASSWORD")
+        val wait = WebDriverWait(driver, Duration.ofSeconds(15))
+        driver.get(baseUrl)
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Login"))).click()
+        val modal = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".modal-dialog"))
+        )
+        modal.findElement(By.id("loginEmail")).sendKeys(email)
+        modal.findElement(By.id("loginPassword")).sendKeys(password)
+        modal.findElement(By.cssSelector("button[type='submit']")).click()
+        wait.until(ExpectedConditions.invisibilityOf(modal))
+        driver.get("${baseUrl}/protected/uploads")
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.id("upload-flights-button")
+            )
+        ).click()
+        val fileInput = wait.until(
+            ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("input[type='file']")
+            )
+        )
+        val filePath = Paths.get(
+            "src/test/resources/ProximityTestFlights.zip"
+        ).toAbsolutePath().toString()
+        fileInput.sendKeys(filePath)
+        wait.until(
+            ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']"))).click()
+        assertTrue(
+            wait.until(
+                ExpectedConditions.or(
+                    ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")),
+                    ExpectedConditions.textToBePresentInElementLocated(
+                        By.tagName("body"),
+                        "Upload"
+                    )
+                )
+            ) != null,
+            "Expected upload success indication"
+        )
+    }
+
+
 }
