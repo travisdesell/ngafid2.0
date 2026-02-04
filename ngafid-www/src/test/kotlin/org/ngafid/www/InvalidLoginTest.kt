62fccd91f5639eb4d4ab6d2c1ab676b6304e75c7
Andrew Lee <leea7633@gmail.com>
2026-02-04 06:38:31 -0500

    invalid login test setup

--- ngafid-www/src/test/kotlin/org/ngafid/www/InvalidLoginTest.kt
+++ ngafid-www/src/test/kotlin/org/ngafid/www/InvalidLoginTest.kt
@@ -1,4 +1,30 @@
 package org.ngafid.www
 
+import org.junit.jupiter.api.*
+import org.openqa.selenium.*
+import org.openqa.selenium.edge.*
+
+
 class InvalidLoginTest {
+
+    companion object {
+        private lateinit var driver: WebDriver
+        private const val baseUrl = "http://localhost:8181/"
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            val options = EdgeOptions()
+            options.addArguments("--headless=new")
+            driver = EdgeDriver(options)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            driver.quit()
+        }
+    }
+
+
 }
