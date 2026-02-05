1cc78ba09738b85e1d0576bf1fc81e46dca1acd3
Andrew Lee <leea7633@gmail.com>
2026-02-04 19:23:13 -0500

    checked to see if login protection tests return false positives, none found

--- ngafid-www/src/test/kotlin/org/ngafid/www/UnauthorizedAccessTest.kt
+++ ngafid-www/src/test/kotlin/org/ngafid/www/UnauthorizedAccessTest.kt
@@ -1,14 +1,33 @@
 package org.ngafid.www
 
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.edge.EdgeDriver
 import org.openqa.selenium.edge.EdgeOptions
 import org.openqa.selenium.support.ui.WebDriverWait
 import java.time.Duration
 
 class UnauthorizedAccessTest {
+    companion object {
+        private lateinit var driver: WebDriver
+        private const val baseUrl = "http://localhost:8181/"
 
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            val options = EdgeOptions()
+            options.addArguments("--headless=new")
+            driver = EdgeDriver(options)
+        }
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            driver.quit()
+        }
+    }
     @Test
     fun unauthenticatedUserRedirectedToLogin() {
         val options = EdgeOptions()
@@ -17,6 +36,8 @@ class UnauthorizedAccessTest {
         val wait = WebDriverWait(driver, Duration.ofSeconds(10))
         try {
             driver.get("http://localhost:8181/protected/uploads")
+            driver.get(baseUrl)
+            println(driver.currentUrl)
 
             wait.until {
                 driver.currentUrl.contains("login") || driver.pageSource.contains("Login")
