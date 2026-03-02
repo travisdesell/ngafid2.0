328e4fbfe0ced22ae2e4bf11ad3102d06aa05554
Andrewlee23 <leea7633@gmail.com>
2026-03-02 09:02:35 -0500

    authentication helper for dynamic testing

---
+++ ngafid-www/src/test/kotlin/org/ngafid/www/e2e/AuthHelper.kt
@@ -0,0 +1,23 @@
+package org.ngafid.www.e2e
+
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+
+object AuthHelper {
+
+    fun login(base: TestBase, email: String, password: String) {
+        val driver = base.driver
+        val wait = base.wait
+        driver.get(base.baseUrl)
+        wait.until(
+            ExpectedConditions.elementToBeClickable(By.linkText("Login"))
+        ).click()
+        val modal = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".modal-dialog"))
+        )
+        modal.findElement(By.id("loginEmail")).sendKeys(email)
+        modal.findElement(By.id("loginPassword")).sendKeys(password)
+        modal.findElement(By.cssSelector("button[type='submit']")).click()
+        wait.until(ExpectedConditions.invisibilityOf(modal))
+    }
+}
