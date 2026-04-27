e309577017152fcaf62f8c4d11ddf7948b6091d2
Aidan Cusack <aidanqsack@gmail.com>
2026-04-27 14:45:42 -0400

    Delete more legacy front-end files

--- ngafid-frontend/src/aggregate_trends.tsx
+++
@@ -1,9 +0,0 @@
-import { TrendsPage } from "./trends";
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const container = document.querySelector("#trends-page");
-if (container) {
-    const root = createRoot(container);
-    root.render(<TrendsPage aggregate_page={true}/>);
-}
