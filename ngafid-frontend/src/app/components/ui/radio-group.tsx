bc3cccbcf6717ed8efe5afbfc862bc482bd9777f
Aidan Cusack <aidanqsack@gmail.com>
2026-04-27 14:38:12 -0400

    Radio Group: Underline text on hover, apply cursor pointer to child elements

--- ngafid-frontend/src/app/components/ui/radio-group.tsx
+++ ngafid-frontend/src/app/components/ui/radio-group.tsx
@@ -10,7 +10,7 @@ const RadioGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
-      className={cn("grid gap-2", className)}
+      className={cn("grid gap-2 **:peer-hover:cursor-pointer **:peer-hover:underline", className)}
       {...props}
       ref={ref}
     />
@@ -26,7 +26,7 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "cursor-pointer aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "cursor-pointer peer aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
