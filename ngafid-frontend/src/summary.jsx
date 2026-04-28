44666e058d9f1d5bf38a584557e744431e6f3e53
Aidan Cusack <aidanqsack@gmail.com>
2026-04-28 13:04:56 -0400

    Delete more legacy scripts/components

--- ngafid-frontend/src/summary.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from "react";
-
-import {SummaryPage} from "./summary_page.js";
-
-const container = document.querySelector("#summary-page");
-const root = createRoot(container);
-root.render(<SummaryPage aggregate={false}/>);
