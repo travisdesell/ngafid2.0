1d964af0c5fa0aac6d2d7d5b4c785eb9fdea309d
Aidan Cusack <aidanqsack@gmail.com>
2026-05-06 17:27:24 -0400

    Delete fleet_trends.tsx

--- ngafid-frontend/src/fleet_trends.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from "react";
-import { TrendsPage } from './trends';
-
-const container = document.querySelector("#trends-page");
-if (container) {
-    const root = createRoot(container);
-    root.render(<TrendsPage aggregate_page={false}/>);
-}
