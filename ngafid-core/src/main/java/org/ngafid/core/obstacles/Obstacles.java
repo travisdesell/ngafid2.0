f54db7b4380781547b58a3bf2d3bb4823a2c65fe
MingFZ0 <mingfengz1678@gmail.com>
2026-06-09 15:45:36 -0400

    Copied the static calculation methods from Airports.java to here as logic of the risk determination will require calculations of the obstacle location in relatively to the position of the aircraft. Modify it later if need be.

---
+++ ngafid-core/src/main/java/org/ngafid/core/obstacles/Obstacles.java
@@ -0,0 +1,77 @@
+/* 
+For the purpose of this class, since Lat./Lon. plus altitude if available is all that we used for obstacles.
+This will just have Lat and Lon, and it will have a similar calculation to that of the Airports.java
+*/
+
+public final class Obstacles {
+    private static final double FT_PER_KM = 3280.84;
+
+    /**
+     * Calculate the shortest distance between a point and a line segment
+     * Modified from:
+     * https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
+     *
+     * @param plat Point latitude
+     * @param plon Point longitude
+     * @param lat1 Latitude of the first point of the line segment
+     * @param lon1 Longitude of the first point of the line segment
+     * @param lat2 Latitude of the second point of the line segment
+     * @param lon2 Longitude of the second point of the line segment
+     * @return the shortest distance between the point and the line segment
+     */
+    public static double shortestDistanceBetweenLineAndPointFt(
+            double plat, double plon, double lat1, double lon1, double lat2, double lon2) {
+        double a = plon - lon1;
+        double b = plat - lat1;
+        double c = lon2 - lon1;
+        double d = lat2 - lat1;
+
+        double dot = a * c + b * d;
+        double lenSq = c * c + d * d;
+        double param = -1;
+
+        if (lenSq != 0) {
+            param = dot / lenSq;
+        }
+
+        double xx;
+        double yy;
+        if (param < 0) {
+            xx = lon1;
+            yy = lat1;
+        } else if (param > 1) {
+            xx = lon2;
+            yy = lat2;
+        } else {
+            xx = lon1 + param * c;
+            yy = lat1 + param * d;
+        }
+
+        double dx = plon - xx;
+        double dy = plat - yy;
+        return Airports.calculateDistanceInFeet(plat, plon, plat + dy, plon + dx);
+    }
+
+    public static double calculateDistanceInKilometer(double lat1, double lon1, double lat2, double lon2) {
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(lon1 - lon2);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1))
+                        * Math.cos(Math.toRadians(lat2))
+                        * Math.sin(lngDistance / 2)
+                        * Math.sin(lngDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return AVERAGE_RADIUS_OF_EARTH_KM * c;
+    }
+
+    public static double calculateDistanceInMeter(double lat1, double lon1, double lat2, double lon2) {
+        return calculateDistanceInKilometer(lat1, lon1, lat2, lon2) * 1000.0;
+    }
+
+    public static double calculateDistanceInFeet(double lat1, double lon1, double lat2, double lon2) {
+        return calculateDistanceInKilometer(lat1, lon1, lat2, lon2) * Airports.FT_PER_KM;
+    }
+}
